7c00cbae80b7904297f223ad920ebc82be49ba28
YoonJunHyeon <qmdldos@gmail.com>
2024-01-29 11:43:30 +0900

    fix : nextjs14에선 getInitialProps는 변경되었다. getServerSideProps로.. 주의 doc : 함수형은 csr 방식에서 많이 사용하게 된다.

--- views/examClassComp.jsx
+++ views/examClassComp.jsx
@@ -6,7 +6,7 @@ import User from "../models/User";
 import Post from "../models/Post";
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
+  static async getServerSideProps({ Component, ctx }) {
     await sequelize.sync(); // 여기서 모델을 동기화합니다.
     return {
       // 기타 필요한 props를 로드합니다.
