b70776597a65d0e4f3e59a4009c86d61bd68aaa8
YoonJunHyeon <qmdldos@gmail.com>
2024-01-29 14:08:59 +0900

    test : sync 테스트파일 작성

---
+++ app/main/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Main() {
+  return (
+    <main className=" flex min-h-screen flex-col items-center justify-center">
+      <div className="w1/2 h-1/2 border border-slate-500">안녕하세요</div>
+    </main>
+  );
+}
