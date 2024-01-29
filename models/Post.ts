2289ade542a0eadcd9a189254a637ae100b15c0a
YoonJunHyeon <qmdldos@gmail.com>
2024-01-29 18:44:46 +0900

    fix : ts로 변환

--- models/Post.ts
+++ models/Post.ts
@@ -1,23 +1,22 @@
-const Sequelize = require("sequelize");
 const sequelize = require("../utils/database");
 
 const Post = sequelize.define("post", {
   post_index: {
-    type: Sequelize.INTEGER,
+    type: DataTypes.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
   post_title: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false,
   },
-  post_content: Sequelize.TEXT,
+  post_content: DataTypes.TEXT,
   post_create_at: {
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.NOW,
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
   },
-  post_update_at: Sequelize.DATE,
+  post_update_at: DataTypes.DATE,
   // user_index 필드는 관계 설정 시 추가됩니다.
 });
 
--- utils/database.ts
+++ utils/database.ts
@@ -1,5 +1,4 @@
-"use client";
-// app/utils/database.js
+// 파일 경로 :  app/utils/database.js
 import { Sequelize } from "sequelize";
 // import dotenv from "dotenv";
 // dotenv.config();
@@ -18,7 +17,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "postgres", // 사용하는 데이터베이스 유형
   host: "localhost",
-  port: "5432",
+  port: 5432, // Change the port value to a number
   database: "yoonjieut_blog",
   username: "postgres",
   password: "root",
