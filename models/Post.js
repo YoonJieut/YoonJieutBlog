637c0a736131b6e8814ba39283d56ca0656f6660
YoonJunHyeon <qmdldos@gmail.com>
2024-01-29 11:19:41 +0900

    feat : 시퀄라이즈를 통한 테이블 작성

--- models/Post.js
+++ models/Post.js
@@ -2,15 +2,23 @@ const Sequelize = require("sequelize");
 const sequelize = require("../utils/database");
 
 const Post = sequelize.define("post", {
-  id: {
+  post_index: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  title: Sequelize.STRING,
-  content: Sequelize.TEXT,
-  // 외래키 설정은 관계 정의 시 추가됩니다.
+  post_title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  post_content: Sequelize.TEXT,
+  post_create_at: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  post_update_at: Sequelize.DATE,
+  // user_index 필드는 관계 설정 시 추가됩니다.
 });
 
 module.exports = Post;
--- models/User.js
+++ models/User.js
@@ -1,16 +1,27 @@
+// User 테이블 정의
 const Sequelize = require("sequelize");
 const sequelize = require("../utils/database");
 
 const User = sequelize.define("user", {
-  id: {
+  user_index: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  email: Sequelize.STRING,
+  user_id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  user_pw: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  user_command: Sequelize.STRING,
+  user_admin_status: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 });
 
 module.exports = User;
