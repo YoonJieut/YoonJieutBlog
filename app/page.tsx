6d69941616fef06c181655b0477270d5cb69b196
YoonJunHyeon <qmdldos@gmail.com>
2024-01-31 17:50:01 +0900

    test : 커스텀 클래스 추가 시도,  실패

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
 import { CustomInputArea } from "@/views/welcomePart/CustomInputArea";
+import { ResponseBox } from "@/views/welcomePart/ResponseBox";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flexCenter min-h-screen flex-col ">
+      <ResponseBox />
       <CustomInputArea />
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,28 +9,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // flexCenter 클래스 정의
+      flexCenter: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       },
-      colors: {
-        "collection-1-color": "var(--collection-1-color)",
-      },
-      backgroundColor: {
-        "zinc-900": "#1C1C1C", // Zinc 900 색상
-        "stone-300": "#CDCDCD", // Stone 300 색상
-        "zinc-300": "#D9D9D9", // Zinc 300 색상
-      },
-      textColor: {
-        "zinc-900": "#1C1C1C", // Zinc 900 색상
-        "stone-300": "#CDCDCD", // Stone 300 색상
-      },
-      borderColor: {
-        "zinc-900": "#1C1C1C", // Zinc 900 색상
-      },
+    },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    },
+    colors: {
+      "collection-1-color": "var(--collection-1-color)",
+    },
+    backgroundColor: {
+      "zinc-900": "#1C1C1C", // Zinc 900 색상
+      "stone-300": "#CDCDCD", // Stone 300 색상
+      "zinc-300": "#D9D9D9", // Zinc 300 색상
+    },
+    textColor: {
+      "zinc-900": "#1C1C1C", // Zinc 900 색상
+      "stone-300": "#CDCDCD", // Stone 300 색상
+    },
+    borderColor: {
+      "zinc-900": "#1C1C1C", // Zinc 900 색상
     },
   },
   plugins: [],
+  extend: {
+    classes: {
+      flexCenter: "flex justify-center items-center",
+    },
+  },
 };
 export default config;
