addf7d8e322784e3d3a4efa5671ec6bf211fa08f
YoonJunHyeon <qmdldos@gmail.com>
2024-01-31 16:26:21 +0900

    faet : svgr 플러그인 설치, config 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config; // Add this line to return the modified config object
+  },
+};
 
 export default nextConfig;
