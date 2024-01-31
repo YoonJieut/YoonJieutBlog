af30e1d5732b362825f84f42c02ee3c5c941e6c5
YoonJunHyeon <qmdldos@gmail.com>
2024-01-31 15:21:13 +0900

    try : config 파일 수정 및  커스텀 명령어 추가
    버그인지 대괄호를 사용하는 동적 클래스가 작성되지 않는다. 이에 대해 조사하는 중이다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,18 @@ const config: Config = {
       colors: {
         "collection-1-color": "var(--collection-1-color)",
       },
+      backgroundColor: {
+        "zinc-900": "#1C1C1C", // Zinc 900 색상
+        "stone-300": "#CDCDCD", // Stone 300 색상
+        "zinc-300": "#D9D9D9", // Zinc 300 색상
+      },
+      textColor: {
+        "zinc-900": "#1C1C1C", // Zinc 900 색상
+        "stone-300": "#CDCDCD", // Stone 300 색상
+      },
+      borderColor: {
+        "zinc-900": "#1C1C1C", // Zinc 900 색상
+      },
     },
   },
   plugins: [],
--- views/testing/CustomInputArea.tsx
+++ views/testing/CustomInputArea.tsx
@@ -1,14 +1,15 @@
+"use client";
 import React from "react";
 
 export const CustomInputArea = (): JSX.Element => {
   return (
-    <div className="inline-flex items-center gap-[37px] relative">
+    <div className="items-center gap-[37px] relative flex">
       <img
         className="relative w-[98.31px] h-[138px]"
         alt="Text cursor"
-        src="text-cursor.png"
+        src="/images/textCursor.png"
       />
-      <div className="relative w-fit font-normal text-[#cdcdcd] text-[90px] tracking-[0] leading-[normal]">
+      <div className="relative w-fit font-normal text-[#cdcdcd] text-[90px] tracking-tighter leading-normal">
         관리자 특별 커맨드
       </div>
     </div>
