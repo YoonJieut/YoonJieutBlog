b43170ff4b950f41e010b1ef058bf6e1a5ea5d71
YoonJunHyeon <qmdldos@gmail.com>
2024-02-05 19:14:30 +0900

    테스트 테이블 생성 마이그레이션 추가

--- migrations/20240205090758-create-test-table.js
+++ migrations/20240205090758-create-test-table.js
@@ -1,22 +1,40 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("test", {
+      test_index: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      test_title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      test_content: {
+        type: Sequelize.STRING,
+        allowNull: true, // 내용이 없을 수도 있으므로 null 허용
+      },
+      // Sequelize는 기본적으로 createdAt과 updatedAt 컬럼을 요구합니다.
+      // 이 컬럼들을 명시적으로 추가하지 않으면, 모델과 마이그레이션 간의 불일치로 인해 오류가 발생할 수 있습니다.
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("test");
+  },
 };
