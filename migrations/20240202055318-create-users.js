7ecaa99bc89c0f707c77882d6cf3ba7231fdaaf3
YoonJunHyeon <qmdldos@gmail.com>
2024-02-05 19:26:53 +0900

    docs : 마이그레이션 파일 생성

--- migrations/20240202055318-create-users.js
+++ migrations/20240202055318-create-users.js
@@ -1,3 +1,5 @@
+// migration 파일은 데이터베이스의 스키마를 변경하기 위한 명령을 정의한 파일이다.
+// migration/20240202055318-create-users.js 파일은 users 테이블을 생성하는 명령을 정의한 파일이다.
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
