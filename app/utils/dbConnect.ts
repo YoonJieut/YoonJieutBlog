e1a1fd141f892fa5978849c8d4e48bdbbc4a9b41
YoonJunHyeon <qmdldos@gmail.com>
2024-02-07 17:00:05 +0900

    feat : next.config.js로 파일명 변경

--- app/utils/dbConnect.ts
+++ app/utils/dbConnect.ts
@@ -2,19 +2,21 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-connection.connect((err: Error) => {
-  if (err) {
-    console.error("Database connection failed: ", err);
-    return;
-  }
-  console.log("Connected to the MySQL server.");
-});
+// 데이터베이스 연결 풀을 사용하여 쿼리를 실행하는 함수
+const dbQuery = (query: string, values?: any[]) => {
+  return new Promise((resolve, reject) => {
+    pool.query(query, values, (error, results) => {
+      if (error) reject(error);
+      else resolve(results);
+    });
+  });
+};
 
-export default connection;
+export default dbQuery;
