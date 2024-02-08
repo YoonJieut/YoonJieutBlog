d58df379910501434ce149a0c20bb6304173e946
YoonJunHyeon <qmdldos@gmail.com>
2024-02-08 14:17:49 +0900

    interface : postsTable 관련 인터페이스 작성

---
+++ app/_interfaces/PostTableProps.ts
@@ -0,0 +1,22 @@
+export interface PostTableProps {
+  id: number;
+  title: string;
+  content: string;
+  authorId: number;
+}
+
+export interface PostIdProps {
+  id: number;
+}
+
+export interface PostTitleProps {
+  title: string;
+}
+
+export interface PostContentProps {
+  content: string;
+}
+
+export interface PostAuthorIdProps {
+  authorId: number;
+}
