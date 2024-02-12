6bbd60c19b2d0642c8610ffa379e23852cc2eb64
YoonJunHyeon <qmdldos@gmail.com>
2024-02-12 20:13:15 +0900

    feat : 유틸 함수 적용

--- app/(pages)/posts/[postId]/page.tsx
+++ app/(pages)/posts/[postId]/page.tsx
@@ -2,37 +2,21 @@
 // 해당 컴포넌트는 게시물 상세 페이지를 구성합니다.
 // 목표 : ssr 방식으로 작성해본다.
 
+import fetchJSON from "@/app/utils/fetchJson";
+import urlMaker from "@/app/utils/urlMaker";
+
 // todo1. : url 파라미터로 게시물 ID를 받아 해당 게시물을 조회합니다.
 // todo2. : 게시물 조회 API를 호출합니다.
 // todo3. : 게시물 정보를 표시합니다.
-
-async function getPostsPostId(postId: string) {
-  const res = await fetch(`http://localhost:3000/api/posts/${postId}`); // Modify the fetch URL to include the full API endpoint
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    console.error("Error fetching post:", res.status, res.statusText);
-  }
-  return res.json();
-}
 
 export default async function PostDetailPage({
   params: { postId },
 }: {
   params: { postId: string };
 }) {
-  const post = await getPostsPostId(postId);
+  const post = await fetchJSON(urlMaker(`/api/posts/${postId}`));
   console.log("post : ", post);
-  /**
-   * [
-      {
-        id: 1,
-        title: 'First Post',
-        content: 'This is the content of the first post.',
-        authorId: 1,
-        createdAt: '2024-02-07T06:46:17.000Z'
-      }
-    ]
-   */
+
   return (
     <>
       <h1>testing : {postId}</h1>
