7a7f61b0e0c72b0cfda94fcdddb52eb2c031dcaa
YoonJunHyeon <qmdldos@gmail.com>
2024-02-14 17:18:04 +0900

    test : via.placeholder 세팅해보기

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     });
     return config; // Add this line to return the modified config object
   },
+  images: {
+    domains: ["via.placeholder.com"],
+  },
 };
 
 export default nextConfig;
