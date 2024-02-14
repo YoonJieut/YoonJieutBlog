80767c557c49563469e4df365f0a5dd96fdf11d9
YoonJieut <qmdldos@gmail.com>
2024-02-15 02:28:49 +0900

    feat : rest 파라미터 사용법을 더 활용해보자
    ts에 대한 속성들에 대해 모르는 것이 많아 제약이 많은 것 같다.

--- app/(pages)/main/page.tsx
+++ app/(pages)/main/page.tsx
@@ -1,9 +1,13 @@
 import ImgCard from "@/app/components/ui/ImgCard";
+import ModalBtn from "../../components/ui/ModalBtn";
+
+// 모달버튼은 이벤트를 전달할 때, 부모 client 컴포넌트로 교체해야 한다.
 
 export default function MainPage() {
   return (
     <>
       <ImgCard imgUrl="/images/textCursor.png" imgTitle="dksd" imgSub="sub" />
+      <ModalBtn>모달버튼</ModalBtn>
     </>
   );
 }
--- app/components/ui/ModalBtn.tsx
+++ app/components/ui/ModalBtn.tsx
@@ -1,6 +1,20 @@
-const ModalBtn = ({ children, ...props }) => {
+"use client";
+import React, { ReactNode, ButtonHTMLAttributes } from "react";
+
+// type ModalBtnProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+//   children: ReactNode;
+// };
+
+interface ModalBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+const ModalBtn = ({ children, ...props }: ModalBtnProps) => {
   return (
-    <button className="modal-btn" {...props}>
+    <button
+      className="modalBtn flex justify-center items-center space-x-10"
+      {...props}
+    >
       {children}
     </button>
   );
