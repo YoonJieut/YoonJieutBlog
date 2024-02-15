e18bd47bc90c8b27df53e0c0723260758cc63478
YoonJunHyeon <qmdldos@gmail.com>
2024-02-15 12:39:55 +0900

    fix : component Props 추가
    이미지 태그의 크기를 조절할 수 있도록 조정했다.

--- app/components/ui/ImgCard.tsx
+++ app/components/ui/ImgCard.tsx
@@ -4,6 +4,8 @@ interface ImgCardProps {
   imgUrl: string;
   imgTitle: string;
   imgSub: string;
+  imgWidth: string | number;
+  imgHeight: string | number;
   className?: string;
 }
 
@@ -11,17 +13,28 @@ const ImgCard: React.FC<ImgCardProps> = ({
   imgUrl,
   imgTitle,
   imgSub,
+  imgWidth,
+  imgHeight,
   className,
 }) => {
   const classNameProps = `imgCard ${className}`;
   return (
     <div className={classNameProps}>
-      <div className="imgArea">
-        <Image src={imgUrl} alt={imgTitle} width={200} height={200} />
+      <div className="imgArea flex justify-center items-center">
+        <Image
+          src={imgUrl}
+          alt={imgTitle}
+          width={imgWidth}
+          height={imgHeight}
+        />
       </div>
-      <ul className="imgSubBar flex justify-between items-center h-8 outline outline-1 ">
-        <li className="imgSubBarTitle text-left">{imgTitle}</li>
-        <li className="imgSubBarSub text-right">{imgSub}</li>
+      <ul className="imgSubBar flex justify-between items-center h-8 ">
+        <li className="imgSubBarTitle text-left opacity-45  active:opacity-100 duration-200">
+          {imgTitle}
+        </li>
+        <li className="imgSubBarSub text-right opacity-45 active:opacity-100 duration-200">
+          {imgSub}
+        </li>
       </ul>
     </div>
   );
