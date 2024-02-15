6f68e108c3bb8dc5c6f3d8540a331403a21f928c
YoonJunHyeon <qmdldos@gmail.com>
2024-02-15 15:07:17 +0900

    feat : 게시물을 조회하여 배열과 단어들을 셔플하는 컴포넌트 생성

--- app/(pages)/main/@postListContainer/page.tsx
+++ app/(pages)/main/@postListContainer/page.tsx
@@ -7,9 +7,11 @@ import {
   PostIdProps,
   PostTitleProps,
 } from "@/app/_interfaces/PostTableProps";
-import Link from "next/link";
+// import Link from "next/link";
+import { shuffleArray } from "@/app/utils/suffleArray";
+import fetchJSON from "@/app/utils/frontend/fetchJSON";
 
-export interface Post
+interface Post
   extends PostIdProps,
     PostTitleProps,
     PostContentProps,
@@ -19,44 +21,56 @@ export default function PostsListContainer() {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    fetch("api/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error(error));
+    fetchJSON("api/posts")
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching posts : ", error);
+      });
   }, []);
+  const shuffledPosts = shuffleArray(posts);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Content</th>
-          <th>Author ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        {posts.map((post) => (
-          <tr key={post.id}>
-            <td>
-              <Link href={`/posts/${post.id}`} key={post.id} className="block">
-                {post.id}
-              </Link>
-            </td>
-            <td>
-              <Link href={`/posts/${post.id}`} key={post.id} className="block">
-                {post.title}
-              </Link>
-            </td>
-            <td>
-              <Link href={`/posts/${post.id}`} key={post.id} className="block">
-                {post.content}
-              </Link>
-            </td>
-            <td>{post.authorId}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <ul className="py-20 flex flex-col space-y-7 text-xl">
+      {shuffledPosts.slice(0, 5).map((post) => (
+        <li key={post.id} className="flex space-x-4">
+          {shuffleArray([post.title, post.content, post.authorId]).map(
+            (item, index) => {
+              // 섞은 배열들을 span으로 감싸면서 스타일링을 적용
+              let style = {};
+              if (typeof item === "string") {
+                if (item === post.title) {
+                  style = { fontWeight: "bold" };
+                } else if (item === post.content) {
+                  style = {
+                    fontSize: "0.9rem",
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                  };
+                }
+              } else if (item === post.authorId) {
+                style = { fontStyle: "italic", fontSize: "0.9rem" };
+              }
+              // map에 대한 리턴값
+              return (
+                <span
+                  key={index}
+                  style={style}
+                  className="outline outline-1 p-2"
+                >
+                  {item === post.authorId
+                    ? item === 1
+                      ? "작성자 : Admin"
+                      : "작성자 : User"
+                    : item}
+                </span>
+              );
+            }
+          )}
+        </li>
+      ))}
+    </ul>
   );
 }
