59279ce70f7632a99cb54435ea1e478d161750d2
YoonJunHyeon <qmdldos@gmail.com>
2024-02-16 13:07:06 +0900

    feat : 병렬 로딩을 위한 라우팅 변경

--- app/(pages)/admin/@newPosts/page.tsx
+++ app/(pages)/admin/@newPosts/page.tsx
@@ -5,6 +5,7 @@
 
 import postFetchJSON from "@/app/utils/frontend/postFetchJSON";
 import { useState } from "react";
+
 export default function NewPostsPage() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
