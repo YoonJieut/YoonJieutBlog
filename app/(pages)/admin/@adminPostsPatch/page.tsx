35c92aa5a4436b3c96fb5df384619d4325b5c029
YoonJunHyeon <qmdldos@gmail.com>
2024-02-19 17:46:27 +0900

    Issue : 표시 렌더링은 되지만 추후 다른 li를 클릭해도 컴포넌트가 변환되지 않는 형태를 발견

--- app/(pages)/admin/@adminPostsPatch/page.tsx
+++ app/(pages)/admin/@adminPostsPatch/page.tsx
@@ -1,26 +1,31 @@
-// app/(pages)/posts/page.tsx
-// 해당 페이지는 게시물 목록을 보여주는 페이지입니다.
+// app/(pages)/admin/@adminPostsPatch/page.tsx
+// 해당 페이지는 게시물을 수정하는 페이지입니다.
 
 "use client";
 
+import { useState } from "react";
 import { Post } from "@/app/_interfaces/PostTableProps";
 import Btn from "@/app/components/Atom/Btn";
 import customFetchJSON from "@/app/utils/frontend/customFetchJSON";
 
-// Props 타입을 정의하여 TypeScript의 타입 체킹 기능을 활용합니다.
-interface PatchPostsPageProps {
-  addEvent: (newPost: Post) => void; // addEvent는 새로운 Post 객체를 매개변수로 받는 함수입니다.
-}
+export default function PatchPostsPage({ addEvent, selectedPost }) {
+  const [patchTitle, setTitle] = useState<string>(selectedPost.title);
+  const [patchContent, setContent] = useState<string>(selectedPost.content);
 
-export default function PatchPostsPage({ addEvent }: PatchPostsPageProps) {
-  const onSubmitButtonEvent = async (e: React.FormEvent<HTMLFormElement>) => {
+  // 수정된 내용을 서버로 전송하고, 서버로부터 받은 데이터로 상태 업데이트
+  const onSubmitButtonEvent = async (
+    e: React.FormEvent<HTMLFormElement>,
+    addEvent
+  ) => {
     e.preventDefault();
+
+    patchToPosts({ selectedPost }, patchTitle, patchContent);
   };
 
   return (
     <div>
       <h1>게시물</h1>
-      <p>게시물을 등록하는 페이지입니다.</p>
+      <p>게시물을 수정하는 페이지입니다.</p>
 
       <form onSubmit={onSubmitButtonEvent}>
         <label className="text-2xl font-bold text-gray-800">
@@ -28,7 +33,7 @@ export default function PatchPostsPage({ addEvent }: PatchPostsPageProps) {
           <input
             type="text"
             className="text-zinc-900"
-            value={title}
+            value={patchTitle}
             onChange={(e) => setTitle(e.target.value)}
           />
         </label>
@@ -36,7 +41,7 @@ export default function PatchPostsPage({ addEvent }: PatchPostsPageProps) {
         <label>
           Content:
           <textarea
-            value={content}
+            value={patchContent}
             className="text-zinc-900"
             onChange={(e) => setContent(e.target.value)}
           />
@@ -45,7 +50,7 @@ export default function PatchPostsPage({ addEvent }: PatchPostsPageProps) {
         <Btn
           type="submit"
           className="bg-blue-500 border border-1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
+          onClick={(addEvent) => {
             onSubmitButtonEvent;
           }}
         >
