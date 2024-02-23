24ac80a70aba0bd03d07813bbd462f718d82324d
YoonJunHyeon <qmdldos@gmail.com>
2024-02-23 21:43:54 +0900

    style : 여백 스타일링 시작

--- app/components/main/IntroduceLayout.tsx
+++ app/components/main/IntroduceLayout.tsx
@@ -7,21 +7,16 @@ import Image from "next/image";
 
 export default function IntroduceLayout() {
   return (
-    <div className="introLayout flex justify-between items-center outline outline-1 mb-3 overflow-x-hidden">
-      <div className="introLeft w-1/2 h-[450px] flex justify-center items-center">
+    <div className="introLayout w-full h-screen flex justify-between items-center outline outline-1 mb-3 overflow-x-hidden">
+      <div className="w-1/6 h-full"></div>
+      <div className="introLeft w-1/6 h-[450px] flex-col flex justify-center items-start">
         <Image src="" alt="Profile" width={300} height={400} />
-      </div>
-      <div className="introRight w-1/2 h-[450px] flex justify-end items-center ">
-        <div className="w-2/3 h-fit">
-          <h3 className="text-left w-full mb-2">나를 소개합니다.</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Necessitatibus placeat dolorem quos nulla. Ipsam eos molestiae
-            explicabo amet pariatur cupiditate magni, fugit rerum id omnis
-            facilis facere, nulla itaque aut.
-          </p>
+        <div className="w-full h-fit">
+          <h2 className="text-left w-full mb-2">나를 소개합니다.</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
         </div>
       </div>
+      <div className="introRight w-5/6 h-[450px] flex justify-end items-center "></div>
     </div>
   );
 }
