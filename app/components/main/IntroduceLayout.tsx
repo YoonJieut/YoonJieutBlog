310017681a12a3655d673021a0a0d90505441f7b
YoonJunHyeon <qmdldos@gmail.com>
2024-02-27 13:08:51 +0900

    feat : 일기 편하기 위한 LargeText 컴포넌트 작성

--- app/components/main/IntroduceLayout.tsx
+++ app/components/main/IntroduceLayout.tsx
@@ -4,19 +4,19 @@
 // todo : 이미지 경로 설정할 것
 
 import Image from "next/image";
-import LogoText from "../ui/ResponsiveText";
-import ResponsiveText from "../ui/ResponsiveText";
+import LargeText from "../ui/LargeText";
 
 export default function IntroduceLayout() {
   return (
-    <div className="introLayout w-full h-screen flex justify-between outline outline-1 mb-3 overflow-x-hidden">
+    <div className="introLayout w-full h-screen flex justify-between outline outline-1 mb-3 overflow-hidden">
       <div className="introLeft w-1/6 h-full flex-col flex OutLine relative">
         <div className="introTextArea w-full h-1/4 absolute left-full flex items-end">
-          안녕하세요! 저는 윤준현이라고 합니다. 현재 프론트엔드 개발자
-          지망생이며 주로 쓰는 기술은 Reat, Next.js, express 등 입니다.
+          안녕하세요! 저는 윤준현이라고 합니다. <br />
+          현재 프론트엔드 개발자 지망생이며 <br />
+          주로 쓰는 기술은 Reat, Next.js, express 등 입니다.
         </div>
         <div className="w-full h-1/3"></div>
-        <div className="imgCardArea w-full h-1/3 OutLine">
+        <div className="imgCardArea w-3/4 h-1/3 OutLine self-end">
           <Image src="" alt="Profile" width={150} height={250} />
           <div className="w-full h-fit">
             <h2 className="text-left w-full mb-2">나를 소개합니다.</h2>
@@ -24,7 +24,10 @@ export default function IntroduceLayout() {
           </div>
         </div>
       </div>
-      <ResponsiveText text="YoonJieut" />
+      <div className="OutLine w-5/6 h-1/4 self-end flex items-end">
+        <LargeText />
+        <LargeText />
+      </div>
     </div>
   );
 }
---
+++ app/components/ui/LargeText.tsx
@@ -0,0 +1,16 @@
+export default function LargeText() {
+  return (
+
+    <p
+      className="Noto w-full h-full overflow-hidden text-nowrap"
+      style={{
+        fontSize: "390px",
+        letterSpacing: "-20px",
+        lineHeight: "0.6",
+        fontWeight: "bold",
+      }}
+    >
+      YoonJieut
+    </p>
+  );
+}
