c9f7a61b9c8b906763208b65c065e66608ae7dc5
YoonJunHyeon <qmdldos@gmail.com>
2024-03-04 17:23:51 +0900

    feat : atom 태그를 활용해서 레이아웃 테스팅하기

---
+++ app/components/ui/testUi/AboutMeTest.tsx
@@ -0,0 +1,13 @@
+import Section from "../../Atom/Section";
+const AboutMeTest: React.FC = () => {
+  return (
+    <div className="aboutMe flex gap-2 w-full h-full">
+      <Section className="w-1/4 h-full OutLine"></Section>
+      <Section className="w-1/4 h-full OutLine"> </Section>
+      <Section className="w-1/4 h-full OutLine"> </Section>
+      <Section className="w-1/4 h-full OutLine"> </Section>
+    </div>
+  );
+};
+
+export default AboutMeTest;
