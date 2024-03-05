a13d1895998beb6f97a121384e1786a25b5886f9
YoonJunHyeon <qmdldos@gmail.com>
2024-03-05 21:52:55 +0900

    feat : temp 파일 생성 및 초기 세팅

---
+++ app/components/ui/Temp/PortfolioTemp.tsx
@@ -0,0 +1,9 @@
+interface PortfolioTempProps {
+  // Define the props for the component here
+}
+
+const PortfolioTemp: React.FC<PortfolioTempProps> = () => {
+  return <div>{/* Add your component's UI elements here */}</div>;
+};
+
+export default PortfolioTemp;
