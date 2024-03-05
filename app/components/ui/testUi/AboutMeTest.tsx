ef4511b689b87e208e70d4069bf36736321a9237
YoonJunHyeon <qmdldos@gmail.com>
2024-03-05 15:00:11 +0900

    style : 여백 수정, 그리고 교육 파트 추가

--- app/components/ui/testUi/AboutMeTest.tsx
+++ app/components/ui/testUi/AboutMeTest.tsx
@@ -1,21 +1,21 @@
-import H5 from "../Atom/H5";
 import Section from "../Atom/Section";
 import TextArea from "../Molecules/TextArea";
 import DetailLine from "@/app/components/ui/DetailLine";
 const AboutMeTest: React.FC = () => {
   return (
     <div className="aboutMe flex gap-2 w-full h-full">
-      <Section className="w-1/4 h-full OutLine p-4 flex flex-col gap-7">
+      <Section className="w-1/4 h-full OutLine p-4 flex flex-col gap-10">
         <TextArea h1Text="윤준현 :)" textAreaClassName="flex flex-col gap-2">
           <p>010 - 4412 - 3007</p>
           <p>현 재 지 역 : 대전</p>
           <p>yoonjieut@Gmail</p>
           <p>GitHub: YoonJieut</p>
+          <p>1996.06.11</p>
         </TextArea>
-        <TextArea h3Text="기술 스택" textAreaClassName="flex flex-col gap-1">
+        <TextArea h3Text="기술" textAreaClassName="flex flex-col gap-1">
           <p>FrontEnd : React, TypeScript, JavaScrpt, HTML, CSS</p>
           <p>BackEnd : Node.js, Express</p>
-          <p>DataBase : MySQL, MongoDB</p>
+          <p>DataBase : MySQL, MariaDB, MongoDB</p>
           <p>ETC : Git</p>
         </TextArea>
         <TextArea h3Text="인적 사항" textAreaClassName="flex flex-col gap-4">
@@ -33,9 +33,19 @@ const AboutMeTest: React.FC = () => {
               1종보통운전면허 (2015.03) - 경찰청(운전면허시험관리단)
             </p>
           </span>
-          <span className="flex flex-col">
-            <H5 text="수료" />
-            <DetailLine className="w-1/12" />
+          <span className="flex flex-col pt-4">
+            <TextArea h5Text="교육" textAreaClassName="flex flex-col gap-2">
+              <p>
+                지역인재양성을 위한 IaaS기반 웹 서비스
+                <br />
+                (Reat,Node,Next,MLOps)개발자 과정 <br />
+                2023.08 ~ 2024.03
+              </p>
+              <p>
+                웹 앱 및 프론트엔드개발자(react) <br />
+                2022.10 ~ 2023.03
+              </p>
+            </TextArea>
           </span>
         </TextArea>
       </Section>
