061908a6c39f74e6c71f12cf1087854bbbce1979
YoonJunHyeon <qmdldos@gmail.com>
2024-03-05 12:46:50 +0900

    feat : molecules 컴포넌트 생성
    h1,h3태그를 사용할 수 있으며 atom 컴포를 적극 활용했다.

--- app/components/ui/testUi/AboutMeTest.tsx
+++ app/components/ui/testUi/AboutMeTest.tsx
@@ -3,17 +3,17 @@ import H3 from "../Atom/H3";
 import Section from "../Atom/Section";
 import DetailLine from "../DetailLine";
 import Div from "../Atom/Div";
+import TextArea from "../Molecules/TextArea";
 const AboutMeTest: React.FC = () => {
   return (
     <div className="aboutMe flex gap-2 w-full h-full">
       <Section className="w-1/4 h-full OutLine p-4">
-        <H1 text="윤준현 :)" />
-        <DetailLine className="w-1/6" />
-        <Div></Div>
-        <p>010 - 4412 - 3007</p>
-        <p>현 재 지 역 : 대전</p>
-        <p>yoonjieut@Gmail</p>
-        <p>GitHub: YoonJieut</p>
+        <TextArea h1Text="윤준현 :)" textAreaClassName="flex flex-col gap-2">
+          <p>010 - 4412 - 3007</p>
+          <p>현 재 지 역 : 대전</p>
+          <p>yoonjieut@Gmail</p>
+          <p>GitHub: YoonJieut</p>
+        </TextArea>
 
         <H3 text="학력" />
         <DetailLine className="w-1/6" />
