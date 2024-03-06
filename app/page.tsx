6f8f43ca6c8331b4da04f39022fed3880cf8040a
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 17:28:18 +0900

    feat : 컴포넌트 정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,6 @@
-// import Image from "next/image";
-import { CustomInputArea } from "@/app/components/ui/Molecules/FetchInput/CustomInputArea";
+// import EntryLayout from "./components/ui/Temp/EntryLayout";
+import EntryLayoutVer2 from "./components/ui/Temp/EntryLayoutVer2";
 
 export default function Home() {
-  return (
-    <main className="w-screen h-screen flex flex-col justify-end items-center overflow-hidden p-4">
-      <div className="font-thin font-serif self-start opacity-50">
-        <p className="font-thin font-serif">해당 페이지는 수정 중입니다.</p>
-        <p className="font-thin font-serif">
-          관리자 페이지 커맨드 : test, admin, 관리자
-        </p>
-        <p className="font-thin font-serif">
-          아무키나 입력하면 메인 포트폴리오 페이지로 접근합니다.
-        </p>
-      </div>
-      <CustomInputArea />
-    </main>
-  );
+  return <EntryLayoutVer2 />;
 }
