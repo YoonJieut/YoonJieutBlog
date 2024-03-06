911d4d40f1d83c3f80bbd0051a16c36738aa6119
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 11:35:06 +0900

    test : 적용 테스트 실패
    원인 : json 파일 그대로 가져오기 때문에 파일 정제가 필요하다

--- app/(pages)/mainTest/portfolio/page.tsx
+++ app/(pages)/mainTest/portfolio/page.tsx
@@ -1,7 +1,26 @@
 import PfList from "@/app/components/ui/Molecules/PfList";
+import pfListData from "@/public/json/pfListData.json";
 
 const PortFolioPage = () => {
-  return <PfList />;
+  console.log("testing - ", pfListData.pfListData);
+  const data = pfListData;
+
+  return (
+    <div className="pfRoot flex w-full h-full gap-4">
+      <div className="w-1/2 h-full"></div>
+      <div className="pfListPart flex flex-col flex-1 w-1/2 h-full justify-center">
+        {/* {pfListData.map((item) => (
+          <PfList
+            key={item.index}
+            index={item.index}
+            stackArray={item.stackArray}
+          >
+            {item.children}
+          </PfList>
+        ))} */}
+      </div>
+    </div>
+  );
 };
 
 export default PortFolioPage;
