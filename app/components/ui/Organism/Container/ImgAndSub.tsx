e2dc61c60b712dc06b93ef8999f6b52b47d6d62f
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 16:18:13 +0900

    feat : ImgAndSub.tsx 컴포넌트 생성
    왼쪽에 imgCard, 오른쪽에 children으로 코드를 작성할 수 있는 컴포넌트 생성

--- app/components/ui/Organism/Container/ImgAndSub.tsx
+++ app/components/ui/Organism/Container/ImgAndSub.tsx
@@ -1,4 +1,43 @@
-const ImgAndSub = () => {
-  return <div>img, sub</div>;
+import {
+  ChildrenProps,
+  ClassNameProps,
+} from "@/app/_interfaces/common/CommomProps";
+import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
+
+interface ImgAndSubProps extends ChildrenProps, ClassNameProps {
+  imgClass: string;
+}
+
+/**
+ * 이미지와 부제목을 포함하는 컨테이너 컴포넌트입니다.
+ *
+ * @param {string} imgClass - 이미지에 적용할 CSS 클래스 이름
+ * @param {ReactNode} children - 이미지 아래에 표시할 자식 요소
+ * @returns {JSX.Element} - 이미지와 부제목을 포함하는 컨테이너 컴포넌트
+ *
+ * @example
+ * ```tsx
+ * <ImgAndSub imgClass="custom-img" >
+ *   <p>부제목 내용</p>
+ * </ImgAndSub>
+ * ```
+ */
+
+const ImgAndSub = ({ imgClass, children }: ImgAndSubProps) => {
+  return (
+    <div className="w-full flex justify-between">
+      <ImgCard
+        imgUrl="/images/textCursor.png"
+        imgTitle="이미지 제목"
+        imgSub="이미지 부제목"
+        imgWidth={250}
+        imgHeight={250}
+        className={imgClass}
+      />
+
+      <div className="imgSubTextArea">{children}</div>
+    </div>
+  );
 };
+
 export default ImgAndSub;
