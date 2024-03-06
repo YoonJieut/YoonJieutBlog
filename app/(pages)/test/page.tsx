f66f5965aeca394edeae4ce17bfdceaeef52f777
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 17:00:24 +0900

    test : test layout 완성

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,12 +1,18 @@
 import ImgAndSub from "@/app/components/ui/Organism/Container/ImgAndSub";
 import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
 import TextAlignLeftOrRight from "@/app/components/ui/Molecules/Text/TextAlignLeftOrRight";
+import ArrowBtns from "@/app/components/ui/Molecules/Buttons/ArrowBtns";
+import H3 from "@/app/components/ui/Atom/Basic/H3";
+import H1 from "@/app/components/ui/Atom/Basic/H1";
 
 const TestPage = () => {
   return (
     <div className="text flex w-screen h-screen overflow-x-hidden">
-      <div className="menuPart w-1/2 h-full OutLine">MenuPart</div>
-      <section className="contentPart w-1/2 flex flex-col">
+      <div className="menuPart w-1/2 h-full OutLine flex flex-col gap-4">
+        <ArrowBtns />
+        <H1 className="Oswald" text="PF1" />
+      </div>
+      <section className="contentPart w-1/2 flex flex-col gap-10">
         <ImgCard
           imgUrl="/images/textCursor.png"
           imgTitle="메인 페이지의 모습"
@@ -19,7 +25,8 @@ const TestPage = () => {
           <p>
             이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
             들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
-            들이 들어갑니다.
+            들이 들어갑니다. 이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미
+            텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.
           </p>
         </TextAlignLeftOrRight>
         <TextAlignLeftOrRight align="right">
@@ -31,7 +38,15 @@ const TestPage = () => {
             더미 텍스트 들이 들어갑니다.
           </p>
         </TextAlignLeftOrRight>
-        <ImgAndSub />
+        <ImgAndSub imgClass="w-2/5">
+          <p>
+            곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
+            들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
+            들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미
+            텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에
+            더미 텍스트 들이 들어갑니다.
+          </p>
+        </ImgAndSub>
       </section>
     </div>
   );
