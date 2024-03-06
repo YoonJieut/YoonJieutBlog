224301a2bd4bd1b91065ba990272d1cc8b9dde66
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 17:28:06 +0900

    style : 스타일링 여백, atom 태그 활용

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -2,8 +2,8 @@ import ImgAndSub from "@/app/components/ui/Organism/Container/ImgAndSub";
 import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
 import TextAlignLeftOrRight from "@/app/components/ui/Molecules/Text/TextAlignLeftOrRight";
 import ArrowBtns from "@/app/components/ui/Molecules/Buttons/ArrowBtns";
-import H3 from "@/app/components/ui/Atom/Basic/H3";
 import H1 from "@/app/components/ui/Atom/Basic/H1";
+import Section from "@/app/components/ui/Atom/Basic/Section";
 
 const TestPage = () => {
   return (
@@ -12,7 +12,7 @@ const TestPage = () => {
         <ArrowBtns />
         <H1 className="Oswald" text="PF1" />
       </div>
-      <section className="contentPart w-1/2 flex flex-col gap-10">
+      <Section className="contentPart w-1/2 flex flex-col gap-10 overflow-x-hidden">
         <ImgCard
           imgUrl="/images/textCursor.png"
           imgTitle="메인 페이지의 모습"
@@ -47,7 +47,24 @@ const TestPage = () => {
             더미 텍스트 들이 들어갑니다.
           </p>
         </ImgAndSub>
-      </section>
+        <TextAlignLeftOrRight align="left">
+          <p>
+            이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
+            들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
+            들이 들어갑니다. 이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미
+            텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.
+          </p>
+        </TextAlignLeftOrRight>
+        <TextAlignLeftOrRight align="right">
+          <p>
+            이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
+            들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
+            들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미
+            텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에
+            더미 텍스트 들이 들어갑니다.
+          </p>
+        </TextAlignLeftOrRight>
+      </Section>
     </div>
   );
 };
