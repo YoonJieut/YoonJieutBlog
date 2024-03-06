7d6458789c1d84560d5dd0467a81665968e9a8cd
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 11:36:25 +0900

    fix : 정제 완료

--- app/(pages)/mainTest/portfolio/page.tsx
+++ app/(pages)/mainTest/portfolio/page.tsx
@@ -3,13 +3,13 @@ import pfListData from "@/public/json/pfListData.json";
 
 const PortFolioPage = () => {
   console.log("testing - ", pfListData.pfListData);
-  const data = pfListData;
+  const data = pfListData.pfListData;
 
   return (
     <div className="pfRoot flex w-full h-full gap-4">
       <div className="w-1/2 h-full"></div>
       <div className="pfListPart flex flex-col flex-1 w-1/2 h-full justify-center">
-        {/* {pfListData.map((item) => (
+        {data.map((item) => (
           <PfList
             key={item.index}
             index={item.index}
@@ -17,7 +17,7 @@ const PortFolioPage = () => {
           >
             {item.children}
           </PfList>
-        ))} */}
+        ))}
       </div>
     </div>
   );
