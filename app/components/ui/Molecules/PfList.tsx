0c78052648e9f83c3a7557709b8c6be6b3b1af64
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 11:34:33 +0900

    feat : PfList 컴포넌트 생성

--- app/components/ui/Molecules/PfList.tsx
+++ app/components/ui/Molecules/PfList.tsx
@@ -4,20 +4,41 @@ import Ul from "../Atom/Ul";
 import StackOuter from "./StackOuter";
 
 interface PortfolioTempProps {
-  // Define the props for the component here
+  index: number;
+  stackArray: string[];
+  children: React.ReactNode;
 }
 
-const PfList: React.FC<PortfolioTempProps> = () => {
+/**
+ * 포트폴리오 리스트 컴포넌트입니다.
+ * @param {number} index - 인덱스 값
+ * @param {string[]} stackArray - 스택 배열
+ * @param {React.ReactNode} children - 자식 요소
+ * @example
+ * // 사용 예시
+ * <PfList index={1} stackArray={['React', 'TypeScript']} >
+ *   <div>내용</div>
+ * </PfList>
+ */
+const PfList: React.FC<PortfolioTempProps> = ({
+  index,
+  stackArray,
+  children,
+}) => {
   return (
-    <div className="OutLine flex w-full h-1/3">
-      <div className="OutLine w-1/3 text-9xl text-right">1</div>
-      <div className="OutLine w-2/3 h-full flex flex-col gap-4 p-4">
+    <div className="OutLine flex w-3/4">
+      {/* clamp() 함수를 사용하여 텍스트 크기가 반응형으로 조절되도록 설정합니다. */}
+      <div className="OutLine flex-1 text-[clamp(2rem,15vw,15rem)] font-bold leading-tight flex items-center justify-end Ambue">
+        {index}
+      </div>
+
+      <div className="OutLine w-2/3 h-full flex flex-col gap-4 p-4 justify-center">
         <H3 text="프로젝트1" />
-        <p>프로젝트 설명</p>
+        {children}
         <Ul className="flex gap-3">
-          {[1, 2, 3].map((item) => (
-            <Li key={item}>
-              <StackOuter divClassName="rounded-full">stack{item}</StackOuter>
+          {stackArray.map((item, index) => (
+            <Li key={index}>
+              <StackOuter divClassName="rounded-full">{item}</StackOuter>
             </Li>
           ))}
         </Ul>
