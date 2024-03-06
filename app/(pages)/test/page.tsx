5af737037bfba92fd683315764f8d3b15db7a5bd
YoonJunHyeon <qmdldos@gmail.com>
2024-03-06 15:45:49 +0900

    test : 컴포넌트 작성, 테스트 완료

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,13 +1,38 @@
-import TextAlignRight from "@/app/components/ui/Molecules/Text/TextAlignRight";
-import TextAlignLeft from "../../components/ui/Molecules/Text/TextAlignLeft";
-import ImgAndSub from "../../components/ui/Organism/Container/ImgAndSub";
+import ImgAndSub from "@/app/components/ui/Organism/Container/ImgAndSub";
+import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
+import TextAlignLeftOrRight from "@/app/components/ui/Molecules/Text/TextAlignLeftOrRight";
 
 const TestPage = () => {
   return (
-    <div className="text flex flex-col">
-      <TextAlignLeft />
-      <TextAlignRight />
-      <ImgAndSub />
+    <div className="text flex w-screen h-screen overflow-x-hidden">
+      <div className="menuPart w-1/2 h-full OutLine">MenuPart</div>
+      <section className="contentPart w-1/2 flex flex-col">
+        <ImgCard
+          imgUrl="/images/textCursor.png"
+          imgTitle="메인 페이지의 모습"
+          imgSub=""
+          imgWidth={400}
+          imgHeight={400}
+          className="w-full OutLine"
+        />
+        <TextAlignLeftOrRight align="left">
+          <p>
+            이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
+            들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
+            들이 들어갑니다.
+          </p>
+        </TextAlignLeftOrRight>
+        <TextAlignLeftOrRight align="right">
+          <p>
+            이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이
+            들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미 텍스트
+            들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에 더미
+            텍스트 들이 들어갑니다.이곳에 더미 텍스트 들이 들어갑니다.이곳에
+            더미 텍스트 들이 들어갑니다.
+          </p>
+        </TextAlignLeftOrRight>
+        <ImgAndSub />
+      </section>
     </div>
   );
 };
