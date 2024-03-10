c8c1e57880631aefc446ef580859b8f2536af94b
YoonJunHyeon <qmdldos@gmail.com>
2024-03-11 03:23:59 +0900

    feat : 디자인 컴포넌트 추가

---
+++ app/components/ui/Organism/SideNav/AdminSideNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AdminSideNav = () => {
+  return (
+    <div className="relative w-[337px] h-[930px] bg-[#f1f1f11a]">
+      <div className="absolute top-[28px] left-[65px] koulen font-normal text-[#d1d1d14c] text-[129px] tracking-[-5.16px] leading-[130.3px] whitespace-nowrap">
+        ADMIN
+      </div>
+    </div>
+  );
+};
---
+++ app/components/ui/design/EntryShadowBar.tsx
@@ -0,0 +1,7 @@
+const EntryShadowBar: React.FC = () => {
+  return (
+    <div className="shadowbar bg-main-1 border-main-1 shadow w-[950px] h-[30px]"></div>
+  );
+};
+
+export default EntryShadowBar;
