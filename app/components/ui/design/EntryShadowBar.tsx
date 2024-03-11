347806e15a2c3c10e7a66465c6abaa972c1bd6f8
YoonJunHyeon <qmdldos@gmail.com>
2024-03-11 16:31:17 +0900

    feat : 디자인 컴포 추가

--- app/components/ui/design/EntryShadowBar.tsx
+++ app/components/ui/design/EntryShadowBar.tsx
@@ -1,6 +1,8 @@
 const EntryShadowBar: React.FC = () => {
   return (
-    <div className="shadowbar bg-main-1 border-main-1 shadow w-[950px] h-[30px]"></div>
+    <div className="shadowbar border-main-1 shadow w-[950px] h-[30px] absolute top-3 left-0 z-0">
+      <div className="w-full h-full bg-main-1 opacity-20"></div>
+    </div>
   );
 };
 
