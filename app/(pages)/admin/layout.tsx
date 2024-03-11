7a39902640729f739e4e1baca6b502b5b86a72ef
YoonJunHyeon <qmdldos@gmail.com>
2024-03-11 19:51:18 +0900

    feat : 어드민 레이아웃 수정 sideNav 컴포넌트 활용

--- app/(pages)/admin/layout.tsx
+++ app/(pages)/admin/layout.tsx
@@ -1,13 +1,12 @@
+import { AdminSideNav } from "../../components/ui/Organism/SideNav/AdminSideNav";
 export default function AdminRootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="adminRoot flex w-screen, h-screen">
-      <section className="outline outline-1 w-1/12 h-full">
-        sidebar area
-      </section>
+    <div className="adminRoot flex w-screen h-screen overflow-hidden">
+      <AdminSideNav />
       <main className="w-full h-full">{children}</main>
     </div>
   );
