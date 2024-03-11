b1d09fa79dfeedf5f5fc2812e4210d1ea66f9755
YoonJunHyeon <qmdldos@gmail.com>
2024-03-12 06:41:35 +0900

    err : router 모듈 찾기 실패

--- app/components/ui/Organism/SideNav/AdminSideNav.tsx
+++ app/components/ui/Organism/SideNav/AdminSideNav.tsx
@@ -1,15 +1,25 @@
-import { redirect } from "next/dist/server/api-utils";
-import React from "react";
+"use client";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export const AdminSideNav = () => {
+  const router = useRouter();
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  if (click === true) {
+    router.push("/");
+  }
+
   return (
     <div className="relative w-[337px] h-full bg-[#f1f1f11a] my-[10px]">
       <div
         className="absolute top-[28px] right-[20px] koulen font-normal text-[#d1d1d14c] text-[129px] tracking-[-5.16px] leading-[130.3px] whitespace-nowrap pointer"
-        onClick={() => {
-          window.location.href = "/";
-        }}
         style={{ writingMode: "vertical-rl", transform: "rotate(180deg)" }}
+        onClick={handleClick}
       >
         ADMIN
       </div>
