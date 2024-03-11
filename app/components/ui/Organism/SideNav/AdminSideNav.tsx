b2808df2785d5c8e61d272ece5787a087d35d67d
YoonJunHyeon <qmdldos@gmail.com>
2024-03-11 19:51:51 +0900

    style : AdminSideNav 수정

--- app/components/ui/Organism/SideNav/AdminSideNav.tsx
+++ app/components/ui/Organism/SideNav/AdminSideNav.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const AdminSideNav = () => {
   return (
-    <div className="relative w-[337px] h-[930px] bg-[#f1f1f11a]">
-      <div className="absolute top-[28px] left-[65px] koulen font-normal text-[#d1d1d14c] text-[129px] tracking-[-5.16px] leading-[130.3px] whitespace-nowrap">
+    <div className="relative w-[337px] h-full bg-[#f1f1f11a] my-[10px]">
+      <div className="absolute top-[28px] right-[20px] koulen font-normal text-[#d1d1d14c] text-[129px] tracking-[-5.16px] leading-[130.3px] whitespace-nowrap">
         ADMIN
       </div>
     </div>
