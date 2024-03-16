490d37910ae609012263aca7f28e946c157ba411
YoonJunHyeon <qmdldos@gmail.com>
2024-03-17 00:50:55 +0900

    feat  : page 컴포넌트 추가 style 투명도 추가,그러나 조금 더 알아봐야 할 듯

--- app/(pages)/mainTest/aboutme/page.tsx
+++ app/(pages)/mainTest/aboutme/page.tsx
@@ -1,9 +1,9 @@
-import AboutMeTest from "@/app/components/ui/design/AboutMeBackText";
+import AboutmePage from "@/app/components/ui/Page/aboutme/AboutmePage";
 
 export default function AboutMeTestPage() {
   return (
     <main className="h-full w-full flexCenter">
-      <AboutMeTest />
+      <AboutmePage />
     </main>
   );
 }
