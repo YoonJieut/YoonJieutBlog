70caa10832a4c6f633ee9d114940b165e0523919
YoonJunHyeon <qmdldos@gmail.com>
2024-03-18 07:53:53 +0900

    feat : background circle

---
+++ app/components/ui/design/IntroduceCircle.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const IntrodcueCircle: NextPage = () => {
+  return (
+    <div className="w-full absolute !m-[0] top-[-3rem] left-[68.75rem] rounded-[50%] bg-dimgray h-[53.875rem]" />
+  );
+};
+
+export default IntrodcueCircle;
