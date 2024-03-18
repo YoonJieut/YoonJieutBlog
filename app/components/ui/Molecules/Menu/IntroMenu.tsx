65aa4bbb38e50ddb201410e547960393bc1c5bef
YoonJunHyeon <qmdldos@gmail.com>
2024-03-18 22:03:49 +0900

    feat : IntroMenu 컴포넌트 생성

--- app/components/ui/Molecules/Menu/IntroMenu.tsx
+++ app/components/ui/Molecules/Menu/IntroMenu.tsx
@@ -1,35 +1,18 @@
 import Li from "@/app/components/ui/Atom/Basic/Li";
 import Ul from "@/app/components/ui/Atom/Basic/Ul";
-import Link from "next/link";
 
 const IntroMenu = () => {
   return (
-    <Ul className="absolute top-[0rem] left-[14.063rem] flex flex-row items-start justify-start gap-[0rem_2.313rem]">
-      <Li>
-        <Link
-          className="relative tracking-[-0.04em] leading-[101%]"
-          href={"/pf/1"}
-        >
-          yoonjieut
-        </Link>
-      </Li>
-      <Li>
-        <Link
-          className="relative tracking-[-0.04em] leading-[101%]"
-          href={"/pf/2"}
-        >
-          erp
-        </Link>
-      </Li>
-      <Li>
-        <Link
-          className="relative tracking-[-0.04em] leading-[101%]"
-          href={"/pf/3"}
-        >
-          chat novel
-        </Link>
-      </Li>
-    </Ul>
+    <div className="IntroMenu flex justify-end w-full gap-4 Li Testing">
+      <div className="w-1/4">
+        <h1 className="koulen text-start titleTypo text-[80px]">Intro</h1>
+      </div>
+      <Ul className="flex items-start justify-start gap-12 koulen text-4xl ">
+        <Li className="Li typo">YoonJieut</Li>
+        <Li className="Li typo">ERP</Li>
+        <Li className="Li typo">CHAT NOVEL</Li>
+      </Ul>
+    </div>
   );
 };
 
