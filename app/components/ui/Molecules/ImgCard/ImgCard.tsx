ca2461c3486c4764b945223a6a88657912e7da2d
YoonJunHyeon <qmdldos@gmail.com>
2024-03-18 19:57:10 +0900

    style : 컴포넌트 flex 값 수정

--- app/components/ui/Molecules/ImgCard/ImgCard.tsx
+++ app/components/ui/Molecules/ImgCard/ImgCard.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Div from "@/app/components/ui/Atom/Basic/Div";
+import Ul from "@/app/components/ui/Atom/Basic/Ul";
+import Li from "@/app/components/ui/Atom/Basic/Li";
 
 interface ImgCardProps {
   imgUrl: string;
-  imgTitle: string;
-  imgSub: string;
+  imgTitle?: string;
+  imgSub?: string;
+  imgAlt: string;
   imgWidth: string | number;
   imgHeight: string | number;
   className?: string;
@@ -14,9 +18,10 @@ interface ImgCardProps {
  * @param {string} imgUrl - 이미지 URL
  * @param {string} imgTitle - 이미지 제목
  * @param {string} imgSub - 이미지 부제목
+ * @param {number} imgAlt - 이미지 설명
  * @param {number} imgWidth - 이미지 너비
  * @param {number} imgHeight - 이미지 높이
- * @param {string} className - 추가 클래스명
+ * @param {string} className? - 추가 클래스명
  * @example
  * ```tsx
  * <ImgCard imgUrl="/path/to/image.jpg" imgTitle="이미지 제목" imgSub="이미지 부제목" imgWidth={300} imgHeight={200} className="custom-class" />
@@ -27,30 +32,26 @@ const ImgCard: React.FC<ImgCardProps> = ({
   imgUrl,
   imgTitle,
   imgSub,
+  imgAlt,
   imgWidth,
   imgHeight,
   className,
 }) => {
   const classNameProps = `imgCard ${className}`;
   return (
-    <div className={classNameProps}>
-      <div className="imgArea flex justify-center items-center">
-        <Image
-          src={imgUrl}
-          alt={imgTitle}
-          width={imgWidth}
-          height={imgHeight}
-        />
-      </div>
-      <ul className="imgSubBar flex justify-between items-center h-8 border-t border-t-zinc-900">
-        <li className="imgSubBarTitle text-left opacity-45  active:opacity-100 duration-200">
+    <Div className={classNameProps}>
+      <Div className="imgArea Testing flex justify-end">
+        <Image src={imgUrl} alt={imgAlt} width={imgWidth} height={imgHeight} />
+      </Div>
+      <Ul className="imgSubBar flex justify-between items-center h-8 border-t border-t-zinc-900">
+        <Li className="imgSubBarTitle text-left opacity-45  active:opacity-100 duration-200">
           {imgTitle}
-        </li>
-        <li className="imgSubBarSub text-right opacity-45 active:opacity-100 duration-200">
+        </Li>
+        <Li className="imgSubBarSub text-right opacity-45 active:opacity-100 duration-200">
           {imgSub}
-        </li>
-      </ul>
-    </div>
+        </Li>
+      </Ul>
+    </Div>
   );
 };
 export default ImgCard;
