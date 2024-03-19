5f4020aeda5d62bc089a610b39b9a28292630f0b
YoonJunHyeon <qmdldos@gmail.com>
2024-03-19 19:24:29 +0900

    style : introduce 포지션 작성

--- app/(pages)/mainTest/introduce/page.tsx
+++ app/(pages)/mainTest/introduce/page.tsx
@@ -17,28 +17,30 @@ export default function IntroduceTestPage() {
           />
         </Link>
       </Section>
-      <Section className="rightIntro relative w-1/2 h-full flex flex-col items-end gap-14">
+      <Section className="rightIntro relative w-1/2 h-full">
         {/* todo : 버튼에 따라 이미지가 달라져야 한다. */}
         {/* 컴포넌트화 해야한다. */}
-        <IntroMenu></IntroMenu>
-        <div className="rightAlignImgCard flex flex-col justify-end items-end gap-4 Testing">
-          <Image
-            src="/images/entry.png"
-            alt="이미지 설명"
-            width={450}
-            height={220}
-            className="ImageCardImg Testing flex justify-end"
-          />
-          <div className="ImageCardTextArea max-w-[600px]">
-            <p className="text-right leading-7 text-[14px]">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              facere eaque error, inventore harum enim ab assumenda placeat
-              possimus officia! Libero distinctio, fugit vero consectetur
-              sapiente officiis a nulla placeat.
-            </p>
+        <div className="content w-full h-full flex flex-col items-end gap-14 mt-[140px]">
+          <IntroMenu></IntroMenu>
+          <div className="rightAlignImgCard flex flex-col justify-end items-end gap-4 Testing">
+            <Image
+              src="/images/entry.png"
+              alt="이미지 설명"
+              width={450}
+              height={220}
+              className="ImageCardImg Testing flex justify-end"
+            />
+            <div className="ImageCardTextArea max-w-[600px]">
+              <p className="text-right leading-7 text-[14px]">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Ratione facere eaque error, inventore harum enim ab assumenda
+                placeat possimus officia! Libero distinctio, fugit vero
+                consectetur sapiente officiis a nulla placeat.
+              </p>
+            </div>
           </div>
+          <IntrodcueCircle />
         </div>
-        <IntrodcueCircle />
       </Section>
     </main>
   );
--- app/components/ui/design/IntroduceCircle.tsx
+++ app/components/ui/design/IntroduceCircle.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const IntrodcueCircle: NextPage = () => {
   return (
-    <div className="IntroduceCircle w-[800px] h-[800px] -z-10 absolute top-[-30px] left-0 rounded-[50%] bg-56 opacity-15" />
+    <div className="IntroduceCircle w-[800px] h-[800px] -z-10 absolute top-[-100px] right-[-120px] rounded-[50%] bg-56 opacity-15" />
   );
 };
 
