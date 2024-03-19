a160298bb0a1b940109f1dd8e852925e585e823e
YoonJunHyeon <qmdldos@gmail.com>
2024-03-19 09:25:52 +0900

    style : circle 전용 스타일 추가

--- app/components/ui/design/IntroduceCircle.tsx
+++ app/components/ui/design/IntroduceCircle.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const IntrodcueCircle: NextPage = () => {
   return (
-    <div className="w-full absolute !m-[0] top-[-3rem] left-[68.75rem] rounded-[50%] bg-dimgray h-[53.875rem]" />
+    <div className="IntroduceCircle w-[800px] h-[800px] -z-10 absolute top-[-30px] left-0 rounded-[50%] bg-56 opacity-15" />
   );
 };
 
