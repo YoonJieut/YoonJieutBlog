618996585ada9359657d86005b91da9d61813330
YoonJieut <qmdldos@gmail.com>
2024-03-23 17:38:38 +0900

    Feat : 상태에 따른 클래스 변경으로 애니메이션을 구현

--- app/components/ui/Molecules/Title/MainBanner.tsx
+++ app/components/ui/Molecules/Title/MainBanner.tsx
@@ -1,25 +1,40 @@
 // MainBanner component
-
-import Link from "next/link";
-
-// 애니메이션 슈도코드
-// 1. mainBannerRed 클래스의 기본 값은 right-3, bottom-7이다.
-// 2. mainBannerGhost 클래스의 기본 값은 right-5, bottom-5이다.
-// 3. mainbannerouter에 마우스가 호버 되면 애니메이션이 실행된다.
-// 4. mainBannerRed, mainBannerGhost의 클래스가 right-0, bottom-0 바뀐다.
+"use client";
+import React, { useState } from "react";
 
 const MainBanner = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+    console.log("isHovered", isHovered);
+  };
+
   return (
-    <div className="mainBannerOuter absolute right-0 bottom-0">
-      <a href="/">
-        <p className="mainBanner text-[16rem] koulen leading-[0.8] tracking-tight absolute right-5 bottoom-5 z-50 font-sub-1 transition-all duration-300 hover:right-0 hover:bottom-0">
+    <div
+      className="mainBannerOuter absolute right-0 bottom-0"
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
+      <p className="mainBanner text-[16rem] koulen leading-[0.8] tracking-tight absolute right-0 bottom-0 z-50 font-sub-1">
+        <a href="/" className="inline-block">
           YoonJieut
-        </p>
-      </a>
-      <p className="mainBannerRed text-[16rem] koulen leading-[0.8] tracking-tight absolute right-3 bottom-7 z-30 font-point opacity-10">
+        </a>
+      </p>
+
+      <p
+        className={`mainBannerRed text-[16rem] koulen leading-[0.8] tracking-tight absolute z-30 font-point opacity-10 transition-all duration-200 ${
+          isHovered ? "right-0 bottom-0" : "right-3 bottom-5"
+        }`}
+      >
         YoonJieut
       </p>
-      <p className="mainBannerGhost text-[16rem] koulen leading-[0.8] tracking-tight absolute right-5 bottom-5 z-40 font-sub-1 opacity-5">
+
+      <p
+        className={`mainBannerGhost text-[16rem] koulen leading-[0.8] tracking-tight absolute z-40 font-sub-1 opacity-5 transition-all duration-200 ${
+          isHovered ? "right-0 bottom-0" : "right-4 bottom-4"
+        }`}
+      >
         YoonJieut
       </p>
     </div>
