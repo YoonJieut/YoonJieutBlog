582913b8c929e4e53331a57b24d55a9c3ee55f09
YoonJieut <qmdldos@gmail.com>
2024-03-25 04:46:19 +0900

    Feat : 컴포넌트 분리

---
+++ app/components/ui/Molecules/ImgCard/IntroImgCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const IntroImgCard = () => {
+  return (
+    <div className="flex flex-col items-end gap-4">
+      <Image
+        src="/images/kakao-talk-202402182120487351.png"
+        alt="개인 포트폴리오 사이트입니다."
+        width={150}
+        height={244}
+      />
+      <p className="text-[12px] leading-5 w-44 text-right">
+        안녕하세요! <br />
+        입체적인 <br />
+        노력을 아끼지 않는 <br />
+        프론트엔드 개발자 윤준현입니다.
+      </p>
+    </div>
+  );
+};
+export default IntroImgCard;
