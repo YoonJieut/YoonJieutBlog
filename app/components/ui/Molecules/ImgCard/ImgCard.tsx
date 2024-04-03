93e85152774066eeeaaaf8e60421b164ee926f5c
YoonJunHyeon <qmdldos@gmail.com>
2024-04-03 20:54:42 +0900

    Refactory : jsdoc와 예시 수정, 리펙토링을 통해 가독성 증가

--- app/components/ui/Molecules/ImgCard/ImgCard.tsx
+++ app/components/ui/Molecules/ImgCard/ImgCard.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import Div from "@/app/components/ui/atom/Basic/Div";
-import Ul from "@/app/components/ui/atom/Basic/Ul";
-import Li from "@/app/components/ui/atom/Basic/Li";
+import Div from "@/app/components/ui/Atom/Basic/Div";
+import Ul from "@/app/components/ui/Atom/Basic/Ul";
+import Li from "@/app/components/ui/Atom/Basic/Li";
+import DetailLine from "@/app/components/ui/design/DetailLine";
 
 interface ImgCardProps {
   imgUrl: string;
+  imgAlt: string;
   imgTitle?: string;
   imgSub?: string;
-  imgAlt: string;
   imgWidth: string | number;
   imgHeight: string | number;
   className?: string;
@@ -16,15 +17,15 @@ interface ImgCardProps {
 /**
  * 이미지 카드 컴포넌트입니다.
  * @param {string} imgUrl - 이미지 URL
+ * @param {number} imgAlt - 이미지 설명
  * @param {string} imgTitle - 이미지 제목
  * @param {string} imgSub - 이미지 부제목
- * @param {number} imgAlt - 이미지 설명
  * @param {number} imgWidth - 이미지 너비
  * @param {number} imgHeight - 이미지 높이
  * @param {string} className? - 추가 클래스명
  * @example
  * ```tsx
- * <ImgCard imgUrl="/path/to/image.jpg" imgTitle="이미지 제목" imgSub="이미지 부제목" imgWidth={300} imgHeight={200} className="custom-class" />
+ * <ImgCard imgUrl="/path/to/image.jpg" imgAlt="이미지 설명" imgTitle="이미지 제목" imgSub="이미지 부제목" imgWidth={300} imgHeight={200} className="custom-class" />
  * ```
  */
 
@@ -40,15 +41,16 @@ const ImgCard: React.FC<ImgCardProps> = ({
   const classNameProps = `imgCard ${className}`;
   return (
     <Div className={classNameProps}>
-      <Div className="imgArea flex justify-center items-center">
+      <Div className="imgArea relative">
         <Image
           src={imgUrl}
           alt={imgAlt}
           width={Number(imgWidth)}
           height={Number(imgHeight)}
         />
+        <DetailLine className="absolute left-0 bottom-0 z-20" />
       </Div>
-      <Ul className="imgSubBar flex justify-between items-center h-8 border-t border-t-zinc-900">
+      <Ul className="imgSubBar flex justify-between items-center h-8 text-[12px] -mt-1">
         <Li className="imgSubBarTitle text-left opacity-45  active:opacity-100 duration-200">
           {imgTitle}
         </Li>
