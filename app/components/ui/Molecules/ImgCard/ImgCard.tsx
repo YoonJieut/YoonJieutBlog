311b7f67ac2e2175a7a9a8d60a7dfb6e1f82398c
YoonJunHyeon <qmdldos@gmail.com>
2024-04-08 12:10:36 +0900

    Refactory : 리터럴과 Detail 컴포넌트 삭제
    try - fill 옵션을 사용할 수 있도록 컴포넌트 수정 중

--- app/components/ui/Molecules/ImgCard/ImgCard.tsx
+++ app/components/ui/Molecules/ImgCard/ImgCard.tsx
@@ -2,20 +2,22 @@ import Image from "next/image";
 import Div from "@/app/components/ui/Atom/Basic/Div";
 import Ul from "@/app/components/ui/Atom/Basic/Ul";
 import Li from "@/app/components/ui/Atom/Basic/Li";
-import DetailLine from "@/app/components/ui/design/DetailLine";
 
 interface ImgCardProps {
   imgUrl: string;
   imgAlt: string;
   imgTitle?: string;
   imgSub?: string;
-  imgWidth: string | number;
-  imgHeight: string | number;
+  imgWidth?: string | number;
+  imgHeight?: string | number;
   className?: string;
+  fill?: boolean;
 }
 
 /**
  * 이미지 카드 컴포넌트입니다.
+ *
+ *
  * @param {string} imgUrl - 이미지 URL
  * @param {number} imgAlt - 이미지 설명
  * @param {string} imgTitle - 이미지 제목
@@ -23,9 +25,15 @@ interface ImgCardProps {
  * @param {number} imgWidth - 이미지 너비
  * @param {number} imgHeight - 이미지 높이
  * @param {string} className? - 추가 클래스명
+ * @param {boolean} fill? - 이미지 꽉 채우기 여부 있으면 Width, Height 무시
  * @example
  * ```tsx
  * <ImgCard imgUrl="/path/to/image.jpg" imgAlt="이미지 설명" imgTitle="이미지 제목" imgSub="이미지 부제목" imgWidth={300} imgHeight={200} className="custom-class" />
+ * ```
+ *
+ * ```
+ *{ fill 속성을 사용할 때 }
+ * <ImgCard imgUrl="/path/to/image.jpg" imgAlt="이미지 설명" imgTitle="이미지 제목" imgSub="이미지 부제목" fill={true} className="custom-class" />
  * ```
  */
 
@@ -37,18 +45,22 @@ const ImgCard: React.FC<ImgCardProps> = ({
   imgWidth,
   imgHeight,
   className,
+  fill,
 }) => {
-  const classNameProps = `imgCard ${className}`;
+  const classNameProps = className ? `${className}` : "imgCard";
   return (
     <Div className={classNameProps}>
       <Div className="imgArea relative">
-        <Image
-          src={imgUrl}
-          alt={imgAlt}
-          width={Number(imgWidth)}
-          height={Number(imgHeight)}
-        />
-        <DetailLine className="absolute left-0 bottom-0 z-20" />
+        {fill ? (
+          <Image src={imgUrl} alt={imgAlt} fill />
+        ) : (
+          <Image
+            src={imgUrl}
+            alt={imgAlt}
+            width={Number(imgWidth)}
+            height={Number(imgHeight)}
+          />
+        )}
       </Div>
       <Ul className="imgSubBar flex justify-between items-center h-8 text-[12px] -mt-1">
         <Li className="imgSubBarTitle text-left opacity-45  active:opacity-100 duration-200">
