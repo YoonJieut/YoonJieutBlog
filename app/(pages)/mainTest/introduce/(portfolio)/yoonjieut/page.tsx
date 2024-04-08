83842ad4ec0da41200eae5e8e737174fd925dd7b
YoonJunHyeon <qmdldos@gmail.com>
2024-04-08 10:53:40 +0900

    Docs : pf 파트 정적 페이지 작성 세팅 완료
    이제 이미지와 내용을 채운다.

--- app/(pages)/mainTest/introduce/(portfolio)/yoonjieut/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/yoonjieut/page.tsx
@@ -8,7 +8,7 @@ import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
 
 // todo arrowbtn에 들어갈 기능 추가
 //  pfRightSection에 내용 추가
-export default function ErpPage() {
+export default function YoonJieutPage() {
   return (
     <div className="w-full h-11/12 flex Tesing">
       <Section className="portfolioLeftSection w-1/2 h-full">
@@ -37,8 +37,8 @@ export default function ErpPage() {
         {/* 사용된 스택과 링크 */}
         <div className="w-full flex justify-between">
           <TextAlign title="사용된 스택과 링크" type="left">
-            <p>{`스택 : TPYESCRIPT, NEXT.JS, EXPRESS, MARIADB `}</p>
-            <p className="m-0 whitespace-pre-wrap">{`의의 : 협업 , Next.JS , DB 설계와 기획 팀 협업 `}</p>
+            <p>{`스택 : TPYESCRIPT, NEXT.JS, EXPRESS, MySQL `}</p>
+            <p className="m-0 whitespace-pre-wrap">{`의의 : 개인 블로그의 초석을 통한 성장, CRUD 학습, 디자인 공부`}</p>
             <p>툴 : Notion, Github, Figma</p>
             <p>&nbsp;</p>
             <p>
@@ -49,7 +49,7 @@ export default function ErpPage() {
                 target="_blank"
               >
                 <span className="[text-decoration:underline]">
-                  윤준현 - 스타크래프트 ERP
+                  윤준현 - 포트폴리오 사이트
                 </span>
               </a>
             </p>
@@ -60,7 +60,7 @@ export default function ErpPage() {
                 href="/FORM-8bb1a4092ee34bab83f8dbd0077fa943?pvs=24"
                 target="_blank"
               >
-                <span className="[text-decoration:underline]">FORM미쳤다</span>
+                <span className="[text-decoration:underline]">개인 피그마</span>
               </a>
             </p>
             <p>
@@ -81,6 +81,67 @@ export default function ErpPage() {
                 <span className="[text-decoration:underline]">Wireframe</span>
               </a>
             </p>
+          </TextAlign>
+        </div>
+        {/* 중앙 정렬 */}
+        <div className="w-full flexCenter">
+          <TextAlign title="" type="center">
+            <p>
+              <b>개인적인 도전으로 독립심과 주관</b>을 갖기 위해{" "}
+              <b>“나만의 디자인을 표현”</b>한 프로젝트입니다.
+              <br />
+              <b>기능</b>으로는 <b>간단한 CRUD 작업</b>과 <b>피그마 활용</b>를
+              중점으로 작업하였습니다.
+            </p>
+          </TextAlign>
+        </div>
+        {/* 오른 정렬 - 주관갖기 어려운 신입 */}
+        <div className="w-full flex justify-between">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 왼쪽 정렬 - 혼란의 과정 */}
+        <div className="w-full flex justify-between">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 중앙 정렬 - 윤준현 연구 */}
+        <div className="w-full flexCenter">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 오른 정렬 - 근거는 과거의 자신 */}
+        <div className="w-full flex justify-between">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 왼쪽 정렬 - 확신은 디자인 연구 */}
+        <div className="w-full flex justify-between">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 이미지 정렬 - 과거 흔적 */}
+        <div className="w-full flex justify-between">
+          {/* 이미지 노력 */}
+          {/* 이미지 수기 */}
+        </div>
+        {/* 나만의 결론 - 전체 이미지 */}
+        <div className="w-full flex justify-between"></div>
+        {/* 오른 정렬 - 아쉬운 점 */}
+        <div className="w-full flex justify-end">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
+          </TextAlign>
+        </div>
+        {/* 왼쪽 정렬 - 의의와 결론 */}
+        <div className="w-full flex justify-between">
+          <TextAlign title="주관 갖기 어려운 신입" type="right">
+            내용
           </TextAlign>
         </div>
       </Section>
