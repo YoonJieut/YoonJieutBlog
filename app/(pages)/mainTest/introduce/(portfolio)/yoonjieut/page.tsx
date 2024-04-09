5adcfa7854958897108c03895dddce3d01beb59c
YoonJunHyeon <qmdldos@gmail.com>
2024-04-09 10:53:22 +0900

    Docs : pf 정적 페이지 완성

--- app/(pages)/mainTest/introduce/(portfolio)/yoonjieut/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/yoonjieut/page.tsx
@@ -27,7 +27,7 @@ export default function YoonJieutPage() {
         {/* 메인 이미지 모습 */}
         <div className="EntryPart w-full">
           <ImgCard
-            imgUrl="/images/erp_mainpage.png"
+            imgUrl="/images/pf_main.png"
             imgAlt="메인 이미지 모습"
             imgTitle="ERP의 메인 페이지"
             imgWidth={950}
@@ -42,9 +42,10 @@ export default function YoonJieutPage() {
             <p className="m-0 whitespace-pre-wrap">{`의의 : 개인 블로그의 초석을 통한 성장, CRUD 학습, 디자인 공부`}</p>
             <p>툴 : Notion, Github, Figma</p>
             <p>&nbsp;</p>
+            {/* todo : a 링크 추가 예정 */}
             <p>
               {`개인 노션 링크 : `}
-              <a
+              <a rel="noreferrer noopener" target="_blank" href="추후 추가 예정">
                 className="whitespace-pre-wrap"
                 href="https://resilient-aster-1dc.notion.site/ERP-63d8c0f6651e4808a677791431d18c61?pvs=4"
                 target="_blank"
@@ -125,21 +126,30 @@ export default function YoonJieutPage() {
         </div>
         {/* 왼쪽 정렬 - 혼란의 과정 */}
         <div className="w-full flex justify-between items-start">
-          <TextAlign title="주관 갖기 어려운 신입" type="left">
-            내용
+          <TextAlign title="혼란의 과정" type="left">
+            <p>인도, 일본 웹 디자인 트랜드, 수 많은 레퍼런스 페이지를 계속 탐구해 나갔지만 <b>많은 혼란만 있었습니다.</b></p>
+            <p>&nbsp;</p>
+            <p><b>초보 디자인이라면 느끼는 막막함’</b></p>
+            <p>내가 뭘 좋아하는 지도 모르는데’라는 마음은 테마를 결정하는 <br />과정에서부터 삐긋 거리기도 했습니다. </p>
+            <p>&nbsp;</p>
+            <p>결국 처음으로 돌아가기로 했습니다.</p>
           </TextAlign>
           <ImgCard
             imgUrl="/images/pf_second.png"
             imgAlt="첫번 째로 만든 레퍼런스들과 레이아웃 사진이 담겨있습니다."
-            imgSub="화려한 인터렉션 중심 스크롤 레이아웃"
+            imgSub="화려한 인터렉션 중심 레퍼런스"
             imgWidth={360}
             imgHeight={360}
           />
         </div>
         {/* 중앙 정렬 - 윤준현 연구 */}
         <div className="w-full flexCenter">
-          <TextAlign title="주관 갖기 어려운 신입" type="right">
-            내용
+          <TextAlign title="디자인 연구 = 윤준현 연구" type="center">
+            <div className="flex flex-col gap-2">
+              <p>많은 고민 속 내린 질문 <b>“내 방을 어떻게 꾸미면 좋을까?”</b></p>
+              <p>그 결과 저는 <b>미니멀리즘에 매력</b>을 느꼈습니다.</p>
+              <p>그리고 그것을 디자인에 반영하기로 결정했습니다.</p>
+            </div>
           </TextAlign>
         </div>
         {/* 오른 정렬 - 근거는 과거의 자신 */}
@@ -151,14 +161,20 @@ export default function YoonJieutPage() {
             imgWidth={160}
             imgHeight={312}
           />
-          <TextAlign title="주관 갖기 어려운 신입" type="right">
-            내용
+          <TextAlign title="근거는 과거의 자신" type="right">
+          <p><b>나는 옛날부터 이런 것을 좋아 했지 </b></p>
+          <p>확신을 가진 것은 다름 없는 과거의 제 자신의 흔적이었습니다.</p>
+          
           </TextAlign>
         </div>
         {/* 왼쪽 정렬 - 확신은 디자인 연구 */}
         <div className="w-full flex justify-between items-start">
-          <TextAlign title="주관 갖기 어려운 신입" type="right">
-            내용
+          <TextAlign title="확신은 연구" type="left">
+          <p><b>‘어떤 의도로 디자인 했지..?’</b></p>
+          <p>무언가 모를 감정을 정의하는 과정은 꽤나 힘듭니다. <br />충격적이게도 작은 인터렉션에도 의미가 있었던 것입니다.</p>
+          <p>&nbsp;</p>
+          <p><b>막막한 가운데 갔던 도서관</b></p>
+          <p>미술 역사책에서 봤던 한 작품은 저에게 깊은 영감을 주었습니다.</p>
           </TextAlign>
           <ImgCard
             imgUrl="/images/pf_pontana.png"
@@ -175,7 +191,6 @@ export default function YoonJieutPage() {
             imgUrl="/images/pf_practice.png"
             imgAlt="레이아웃을 실제로 그리고 재밌게 하기 위해 웃긴 사진들과 노션에 정리하던 이미지가 담겨있습니다."
             imgTitle="연습 작업"
-            imgSub="분야는 가리지 않았습니다."
             imgWidth={300}
             imgHeight={170}
           />
@@ -192,10 +207,13 @@ export default function YoonJieutPage() {
         <div className="w-full h-full flex flex-col gap-12">
           <div className="w-full flexCenter">
             <TextAlign title="나만의 결론 내기" type="center">
-              내용
+              <div className="flex flex-col gap-2">
+                <p>예술이란 <b>의도를 갖고 표현하는 방식을 고민</b>하는 것</p>
+                <p>그렇다면 웹도 다를 것이 없을 것이다</p>
+              </div>
             </TextAlign>
           </div>
-          {/* 이슈 - 이미지 퀄리티가 많이 떨어진다. */}
+          {/* 이슈 todo - 이미지 퀄리티가 많이 떨어진다. */}
           <ImgCard
             imgUrl="/images/pf_figma.png"
             imgAlt="이전 웹 퍼블리셔 과정에 준비하던 pf 이미지가 담겨있습니다."
@@ -205,17 +223,34 @@ export default function YoonJieutPage() {
             className="figmaImgCard h-[400px] w-full"
           />
         </div>
+        {/* 아쉬운점, 결론 문단 */}
+        <div className="flex flex-col gap-20">
         {/* 오른 정렬 - 아쉬운 점 */}
         <div className="w-full flex justify-end">
-          <TextAlign title="주관 갖기 어려운 신입" type="right">
-            내용
+          <TextAlign title="작업 시 아쉬운 점" type="right">
+            <p>이번 작업은 <b>완벽주의라는 틀</b>에 갇혀 많은 생각에 가로막혔습니다.<br />
+            많은 욕심으로 머신러닝 프로덕트와 기술력도 탐냈고,<br />화려한 인터렉션에도 홀려 <b>주관을 흐리기도 했습니다.</b></p>
+            <p>&nbsp;</p>
+            <p>이 현상에 갇힌 것은 저의 <b>“마음 문제”</b>가 가장 크다고 생각합니다. <br />
+            작은 욕심을 원동력으로 많은 작업을 서스럼없이 하게 되었을 때 <br />비로소 성장한다는 것을 느꼈습니다.</p>
           </TextAlign>
         </div>
         {/* 왼쪽 정렬 - 의의와 결론 */}
-        <div className="w-full flex justify-between items-start">
-          <TextAlign title="주관 갖기 어려운 신입" type="right">
-            내용
+        <div className="w-full flex justify-between items-start pb-10">
+          <TextAlign title="의의와 결론" type="left">
+            <p><b>이번 작품에서 가장 주요하게 여긴 것</b></p>
+            <p> 바로 자신에 대한 연구였는데요 많은 사람은 면접자에 대해 그리 궁금해 하지 않는다고들 합니다. </p>
+            
+            <p className="mt-2">하지만 <br />
+            저의 개발자 인생에 있어서 꼭 필요한 과정이었습니다.<br />
+            이후 모든 작업에 자신감을 가져다 주었습니다.</p>
+            <p>&nbsp;</p>
+            <p><b>무엇을 하던 불확실한 현재</b></p>
+            <p>
+            오로지 믿은 것은 저 자신이었습니다. <br />겸손과 성찰에 대한 중요성, 자신에 대한 주관을 세울 수 있는 작품이었습니다.
+            </p>
           </TextAlign>
+        </div>
         </div>
       </Section>
     </div>
