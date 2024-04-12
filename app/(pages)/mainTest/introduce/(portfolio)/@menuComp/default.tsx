b1f3161c63f0a27c6bb35d026f849f051471d2e8
YoonJunHyeon <qmdldos@gmail.com>
2024-04-12 16:08:25 +0900

    Fix : client 컴포넌트를 세팅하고, useRouter를 사용할 수 없었다.
    해결 방법 : 너무 많은 시간을 할애했기 때문에 우선, windowl.loaction.herf로 간단히 해결했다.

--- app/(pages)/mainTest/introduce/(portfolio)/@menuComp/default.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/@menuComp/default.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import Div from "@/app/components/ui/Atom/Basic/Div";
 import H1 from "@/app/components/ui/Atom/Basic/H1";
 import StackOuter from "@/app/components/ui/design/StackOuter";
+import urlParamMaker from "@/app/utils/urlParamMaker";
+import { useEffect } from "react";
 
-const menuCompdefault = () => {
+// todo : 해당 레이아웃의 병렬 컴포넌트가 실행 될 때 마다 url을 검사하여 현재 index를 판단, 이전, 다음 버튼을 누르면 해당 페이지로 이동
+
+const MenuCompDefault = () => {
+  useEffect(() => {
+    const url = window.location.href;
+    const param = urlParamMaker(url);
+    console.log("param", param);
+  }, []);
+
   return (
     <>
-      <p>이건 기본 defualt 페이지 입니다.</p>
       <Div className="flex flex-col gap-2">
         <H1 text="YOONJIEUT" className="koulen font-normal text-8xl" />
         <Div className="StackFlexBox flex space-x-2 ">
@@ -17,4 +28,4 @@ const menuCompdefault = () => {
     </>
   );
 };
-export default menuCompdefault;
+export default MenuCompDefault;
