25e21b0206d293d7b865a538a4debd09f5c53063
YoonJunHyeon <qmdldos@gmail.com>
2024-04-12 16:08:56 +0900

    Docs : 모든 값을 제거하여 page단위의 ui component만 남겼다.

--- app/(pages)/mainTest/introduce/(portfolio)/chatnovel/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/chatnovel/page.tsx
@@ -1,30 +1,6 @@
-import H1 from "@/app/components/ui/Atom/Basic/H1";
-import Section from "@/app/components/ui/Atom/Basic/Section";
-import ArrowBtns from "@/app/components/ui/Molecules/Buttons/ArrowBtns";
-import Div from "@/app/components/ui/Atom/Basic/Div";
-import StackOuter from "@/app/components/ui/design/StackOuter";
 import ChatnovelContent from "@/app/components/ui/Page/portfolio/chatnovel/ChatNovelContent";
 
 // todo arrowbtn에 들어갈 기능 추가
 export default function ChatNovelPage() {
-  return (
-    <div className="w-full h-11/12 flex Tesing">
-      <Section className="portfolioLeftSection w-1/2 h-full">
-        <ArrowBtns />
-        <Div className="flex flex-col gap-2">
-          <H1 text="CHAT NOVEL" className="koulen font-normal text-8xl" />
-          <Div className="StackFlexBox flex space-x-2 ">
-            <StackOuter divClassName="rounded-full">TypeScript</StackOuter>
-            <StackOuter divClassName="rounded-full">Express</StackOuter>
-            <StackOuter divClassName="rounded-full">STUDY</StackOuter>
-            <StackOuter divClassName="rounded-full">JSON</StackOuter>
-            <StackOuter divClassName="rounded-full">CLOSURE</StackOuter>
-          </Div>
-        </Div>
-      </Section>
-      <Section className="portfolioRightSection w-1/2 Testing flex flex-col gap-[300px] overflow-x-hidden overflow-y-scroll">
-        <ChatnovelContent />
-      </Section>
-    </div>
-  );
+  return <ChatnovelContent />;
 }
