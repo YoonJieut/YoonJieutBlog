a6ceeae26340ea89e20352e8e9e2b0230de5abfb
YoonJunHyeon <qmdldos@gmail.com>
2024-04-15 11:04:15 +0900

    Test : useNavigation 커스텀훅 추가 console.log를 통한 디버깅 시도 중
    뭐가 문제일까?

--- app/hooks/portfolio/useNavigation.ts
+++ app/hooks/portfolio/useNavigation.ts
@@ -1,33 +1,37 @@
-import { useRouter } from "next/router";
 import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
+import { useRouter } from "next/navigation";
 
-function useNavigation() {
-  console.log("useNavigation 실행");
+function useNavigation(slut: string | undefined) {
   const router = useRouter();
-  console.log("router", router.pathname);
+  console.log("router - ", router);
+  if (typeof slut === "string") {
+    const currentPageIndex = portfolioMetaData.findIndex(
+      (item) => item.name.toLowerCase() === slut
+    );
+    console.log(currentPageIndex);
 
-  const currentPageIndex = portfolioMetaData.findIndex(
-    (page) => page.path === router.pathname
-  );
-  const prevPageIndex = currentPageIndex > 0 ? currentPageIndex - 1 : null;
-  const nextPageIndex =
-    currentPageIndex < portfolioMetaData.length - 1
-      ? currentPageIndex + 1
-      : null;
+    const prevPageIndex = currentPageIndex > 0 ? currentPageIndex - 1 : null;
+    const nextPageIndex =
+      currentPageIndex < portfolioMetaData.length - 1
+        ? currentPageIndex + 1
+        : null;
 
-  const handlePrevClick = () => {
-    if (prevPageIndex !== null) {
-      router.push(portfolioMetaData[prevPageIndex].path);
-    }
-  };
+    const handlePrevClick = () => {
+      if (prevPageIndex !== null) {
+        router.push(portfolioMetaData[prevPageIndex].path);
+      }
+    };
 
-  const handleNextClick = () => {
-    if (nextPageIndex !== null) {
-      router.push(portfolioMetaData[nextPageIndex].path);
-    }
-  };
+    const handleNextClick = () => {
+      if (nextPageIndex !== null) {
+        router.push(portfolioMetaData[nextPageIndex].path);
+      }
+    };
 
-  return { handlePrevClick, handleNextClick };
+    return { handlePrevClick, handleNextClick };
+  } else {
+    console.error("slut이 undefined 입니다.", slut);
+  }
 }
 
 export default useNavigation;
