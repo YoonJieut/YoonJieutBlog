3732a16f8b9f10f63a0dffa15fae895208515764
YoonJunHyeon <qmdldos@gmail.com>
2024-04-15 11:22:58 +0900

    feat : useNavigation 에러문 추가
    모든 비교는 소문자로 진행하도록 로직 수정 및 맨 뒤로 가도록 수정

--- app/hooks/portfolio/useNavigation.ts
+++ app/hooks/portfolio/useNavigation.ts
@@ -3,25 +3,30 @@ import { useRouter } from "next/navigation";
 
 function useNavigation(slut: string | undefined) {
   const router = useRouter();
-  console.log("router - ", router);
+  console.log(slut);
+  // console.log("router - ", router);
   if (typeof slut === "string") {
     const currentPageIndex = portfolioMetaData.findIndex(
-      (item) => item.name.toLowerCase() === slut
+      (item) => item.name.toLowerCase() === slut.toLowerCase()
     );
-    console.log(currentPageIndex);
-
-    const prevPageIndex = currentPageIndex > 0 ? currentPageIndex - 1 : null;
+    console.log("현재 주소의 index는? - ", currentPageIndex);
+    if (currentPageIndex === -1) {
+      console.error("오류 발생 ! : 위 파라미터는 없는 url입니다.");
+    }
+    const prevPageIndex =
+      currentPageIndex > 0
+        ? currentPageIndex - 1
+        : portfolioMetaData.length - 1;
     const nextPageIndex =
       currentPageIndex < portfolioMetaData.length - 1
         ? currentPageIndex + 1
-        : null;
+        : 0;
 
     const handlePrevClick = () => {
       if (prevPageIndex !== null) {
         router.push(portfolioMetaData[prevPageIndex].path);
       }
     };
-
     const handleNextClick = () => {
       if (nextPageIndex !== null) {
         router.push(portfolioMetaData[nextPageIndex].path);
