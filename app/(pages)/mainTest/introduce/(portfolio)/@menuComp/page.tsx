5e44c76a671a70afef7f4455275a3f56ad26aedd
YoonJunHyeon <qmdldos@gmail.com>
2024-04-15 12:11:35 +0900

    Delete : 모두 초기화 하고 하나의 컴포넌트에서 부터 해본다.

--- app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
@@ -1,38 +1,33 @@
 "use client";
 
-import Div from "@/app/components/ui/Atom/Basic/Div";
-import H1 from "@/app/components/ui/Atom/Basic/H1";
-import StackOuter from "@/app/components/ui/design/StackOuter";
-import useNavigation from "@/app/hooks/portfolio/useNavigation";
-import urlParamMaker from "@/app/utils/urlParamMaker";
+import { useState } from "react";
 
 // todo : 해당 레이아웃의 병렬 컴포넌트가 실행 될 때 마다 url을 검사하여 현재 index를 판단, 이전, 다음 버튼을 누르면 해당 페이지로 이동
 
 const MenuComp = () => {
   console.log("MenuComp");
-  // 현재 url에서 path를 추출하여 현재 index를 커스텀 훅에 제공한다.
-  // const urlString = typeof window !== "undefined" ? window.location.href : "";
-  const urlString = window.location.href;
-  const nowPath = urlParamMaker(urlString);
-  console.log(nowPath);
-  const { handlePrevClick, handleNextClick } = useNavigation(nowPath);
+  // const { nowUrl, setNowUrl } = useState("");
+
+  const handlePrevClick = () => {
+    console.log("handlePrevClick");
+  };
+  const handleNextClick = () => {
+    console.log("handleNextClick");
+  };
 
   return (
     <>
-      <button onClick={handlePrevClick}>이전 페이지</button>
-      <button onClick={handleNextClick}>다음 페이지</button>
-      {/* <ArrowBtns prevOnClick={prevClick} nextOnClick={nextClick} /> */}
-      <Div className="flex flex-col gap-2 mt-40">
-        <H1
-          text="YOONJIEUT"
-          className="koulen font-normal text-8xl leading-tight"
-        />
-        <Div className="StackFlexBox flex space-x-2 ">
-          <StackOuter divClassName="rounded-full">TypeScript</StackOuter>
-          <StackOuter divClassName="rounded-full">NextJS</StackOuter>
-          <StackOuter divClassName="rounded-full">Express</StackOuter>
-        </Div>
-      </Div>
+      <div className="w-full flex justify-between">
+        <button
+          onClick={() => {
+            handlePrevClick;
+          }}
+        >
+          이전 페이지
+        </button>
+
+        <button onClick={handleNextClick}>다음 페이지</button>
+      </div>
     </>
   );
 };
