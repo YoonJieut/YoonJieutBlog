b4b8f0a3ee727c64a050de32cec9139a5b4118ae
YoonJunHyeon <qmdldos@gmail.com>
2024-04-16 13:50:49 +0900

    Refectory : 커스텀훅으로 제작하여 컴포넌트를 분리했다.
    진작에 이렇게 할 걸! 너무 깔끔한 코드!

--- app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
@@ -1,33 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
-import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
 import H1 from "@/app/components/ui/Atom/Basic/H1";
-import urlParamMaker from "@/app/utils/urlParamMaker";
+import useMenuNavigation from "@/app/_hook/portfolio/useMenuNavigation";
+import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
 
 const MenuComp = () => {
-  const [currentMenu, setCurrentMenu] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const router = usePathname();
-
-  useEffect(() => {
-    const currentPath = urlParamMaker(router) ?? "";
-    const nowIndex = portfolioMetaData.findIndex(
-      (data) => data.name.toLowerCase() === currentPath?.toLowerCase()
-    );
-
-    if (nowIndex !== -1) {
-      setCurrentMenu(currentPath);
-      setCurrentIndex(nowIndex);
-    }
-  }, [router]);
-
-  const prevIndex =
-    currentIndex === 0 ? portfolioMetaData.length - 1 : currentIndex - 1;
-  const nextIndex =
-    currentIndex === portfolioMetaData.length - 1 ? 0 : currentIndex + 1;
+  const { currentMenu, prevIndex, nextIndex } = useMenuNavigation();
 
   return (
     <>
@@ -36,9 +15,6 @@ const MenuComp = () => {
         <Link href={portfolioMetaData[nextIndex].path}>다음 페이지</Link>
       </div>
       <H1 text={currentMenu} />
-      <H1 text={currentIndex.toString()} />
-      <H1 text={prevIndex.toString()} />
-      <H1 text={nextIndex.toString()} />
     </>
   );
 };
