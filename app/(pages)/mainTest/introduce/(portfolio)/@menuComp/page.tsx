d307e74ceae1d3f810e0e15ed40c1384827f7bd6
YoonJunHyeon <qmdldos@gmail.com>
2024-04-16 14:57:59 +0900

    Docs : PfItemsMenu 컴포넌트 분리 및 적용
    기존 컴포넌트를 변경, 파라미터 전달까지 테스트 해봤다.

--- app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import Link from "next/link";
-import H1 from "@/app/components/ui/Atom/Basic/H1";
 import useMenuNavigation from "@/app/_hook/portfolio/useMenuNavigation";
-import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
+import PfItemsLayout from "@/app/components/ui/Organism/Portfolio/Items/PfItemsLayout";
 
 const MenuComp = () => {
-  const { currentMenu, prevIndex, nextIndex } = useMenuNavigation();
+  const { currentIndex, prevIndex, nextIndex } = useMenuNavigation();
 
   return (
-    <>
-      <div className="w-full flex justify-between mt-20">
-        <Link href={portfolioMetaData[prevIndex].path}>이전 페이지</Link>
-        <Link href={portfolioMetaData[nextIndex].path}>다음 페이지</Link>
-      </div>
-      <H1 text={currentMenu} />
-    </>
+    <div className="menuPart">
+      <PfItemsLayout
+        prevIndex={prevIndex}
+        nextIndex={nextIndex}
+        currentIndex={currentIndex}
+      />
+    </div>
   );
 };
 
