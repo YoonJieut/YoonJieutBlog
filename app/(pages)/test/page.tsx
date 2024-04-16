bf83dd42bdd2a71bc0a6cc6b7380ba79d1f61209
YoonJunHyeon <qmdldos@gmail.com>
2024-04-16 14:55:31 +0900

    Clean : 컴포넌트 분리, 디렉토리 구조 이동
    재사용하지는 않지만, 분업을 했다라면 이렇게 하지 않았을까? 굳이 컴포넌트를 나누는 이유는 이런 것이다.

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,57 +1,5 @@
-import DotLine from "@/app/components/ui/design/dotLine";
-import MainFooter from "@/app/components/ui/Organism/Footer/MainFooter";
-import EntryShadowBar from "../../components/ui/design/EntryShadowBar";
-import Corner from "@/app/components/ui/design/Corner";
-import { AdminSideNav } from "../../components/ui/Organism/SideNav/AdminSideNav";
-import ImgCard from "@/app/components/ui/Molecules/ImgCard/ImgCard";
-import TextPfSummary from "@/app/components/ui/Molecules/Text/TextPfSummary";
-import PostTitle from "@/app/components/ui/design/PostTitle";
-import AboutMeBackText from "@/app/components/ui/design/AboutMeBackText";
-import ArrowBtns from "@/app/components/ui/Molecules/Buttons/ArrowBtns";
-import PfInfo from "@/app/components/ui/Molecules/Lists/PfInto";
-import TextAlignLeftOrRight from "@/app/components/ui/Atom/Basic/Flex";
-import TextAlign from "@/app/components/ui/Molecules/Text/TextAlign";
-import PfMenu from "../../components/ui/Molecules/Menu/PfMenu";
-
 const TestPage = () => {
-  return (
-    <>
-      <EntryShadowBar />
-      <DotLine />
-      <MainFooter />
-      <Corner />
-      <AdminSideNav />
-      <PostTitle />
-      <AboutMeBackText />
-      <ArrowBtns />
-      <PfInfo title="title" stackArray={["stack1", "stack2"]} />
-      <TextAlignLeftOrRight align="left">
-        <div>left</div>
-      </TextAlignLeftOrRight>
-      <TextAlignLeftOrRight align="right">
-        <div>right</div>
-      </TextAlignLeftOrRight>
-      <TextAlign type="left" title="왼쪽 정렬 제목">
-        왼쪽 정렬 내용 Lorem ipsum dolor sit ame tconsectetur. Eget ultrices
-        arcu sed amet nulla sit est. Tortor magna mi nam accumsan curabitur in
-        turpis. Malesuada tincidunt viverra donec at morbi pharetra sollicitudin
-        auctor ullamcorper. Neque nibh eu at tristique. Neque =
-      </TextAlign>
-      <TextAlign type="right" title="right쪽 정렬 제목">
-        왼쪽 정렬 내용 Lorem ipsum dolor sit ame tconsectetur. Eget ultrices
-        arcu sed amet nulla sit est. Tortor magna mi nam accumsan curabitur in
-        turpis. Malesuada tincidunt viverra donec at morbi pharetra sollicitudin
-        auctor ullamcorper. Neque nibh eu at tristique. Neque =
-      </TextAlign>
-      <TextAlign type="center" title="center 정렬 제목">
-        왼쪽 정렬 내용 Lorem ipsum dolor sit ame tconsectetur. Eget ultrices
-        arcu sed amet nulla sit est. Tortor magna mi nam accumsan curabitur in
-        turpis. Malesuada tincidunt viverra donec at morbi pharetra sollicitudin
-        auctor ullamcorper. Neque nibh eu at tristique. Neque =
-      </TextAlign>
-      <PfMenu />
-    </>
-  );
+  return <>테스트 페이지입니다.</>;
 };
 
 export default TestPage;
