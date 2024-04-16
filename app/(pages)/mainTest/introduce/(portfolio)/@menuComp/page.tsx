300e663b0c81830ebac68b0140aafd5697bca13f
YoonJunHyeon <qmdldos@gmail.com>
2024-04-16 13:39:40 +0900

    Refectory : Next에서 제공하는 훅을 활용하는 것이 더 최적화된 것 같다.
    app라우트 방식에선 navigation으로 호출해야 하며, Pathname을 활용하여 원하는 로직을 편하고 빠르게 완성 할 수 있었다..
    내가 만드는 것보다 찾아서 활용하는 것이 더 프레임워크 단에선 유려하게 작동할 수 있는 것이다.

--- app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
+++ app/(pages)/mainTest/introduce/(portfolio)/@menuComp/page.tsx
@@ -1,96 +1,45 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
 import H1 from "@/app/components/ui/Atom/Basic/H1";
-import urlParamMaker from "@/app/utils/urlParamMaker";
-import { useEffect, useState } from "react";
-
-// 해당 컴포넌트는 pf 페이지의 왼쪽 섹션에 해당하며 병렬 로딩을 구현했다.
-
-// // todo1 : 현재의 url을 참고하여 현재 메뉴를 표시한다.
-// 1. 현재 url에서 "/"을 기준으로 맨 뒤의 문자열을 가져온다.
-// 2. 가져온 문자열을 컴포넌트에 업데이트 한다.
-// todo2 : 이전, 다음 버튼을 클릭하면 이전, 다음 메뉴로 이동한다.
-// 1. 가져온 문자열을 기준으로 현재 메뉴의 index를 반환해본다.
-// 2. portfolioMetaData에 맞춰서 이전, 다음 메뉴로 리디렉션한다.
-// 3. 맨 뒤로 가면 처음으로 돌아오는 로직을 추가한다. (0에서 뒤로 가면 마지막으로 간다 그리고 마지막에서 앞으로 가면 처음으로 간다.)
 
 const MenuComp = () => {
-  const [url, setUrl] = useState("");
   const [currentMenu, setCurrentMenu] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [nextIndex, setNextIndex] = useState(0);
-  const [prevIndex, setPrevIndex] = useState(0);
-  const [isClickable, setIsClickable] = useState(true);
+  const router = usePathname();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsClickable(true);
-    }, 1000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
-  // 현재 url에 따라 인덱스를 반환한다.
-  useEffect(() => {
-    // 랜더링 되면, 현재 주소를 상태로 저장한다.
-    setUrl(window.location.href);
-
-    const param = urlParamMaker(url);
-    setCurrentMenu(param as string); // 타입에러 : 타입을 string으로 강제로 변환했다.
-
-    // 현재 param을 기준으로 현재 메뉴의 index를 반환한다.
+    const currentPath = router.split("/").pop() ?? "";
     const nowIndex = portfolioMetaData.findIndex(
-      (data) => data.name.toLowerCase() === param
+      (data) => data.name.toLowerCase() === currentPath?.toLowerCase()
     );
-    // 찾은 url이 없을 때, 오류 처리
-    if (nowIndex === -1) {
-      setCurrentIndex(0);
-      return;
-    }
-    console.log("nowIndex", nowIndex);
-    setCurrentIndex(nowIndex);
-  }, [url]);
 
-  // 현재 인덱스에 따른 다음, 이전 인덱스를 반환.
-  useEffect(() => {
-    console.log("MenuComp useEffect3");
-    const prev =
-      currentIndex == 0 ? portfolioMetaData.length - 1 : currentIndex - 1;
-    const next =
-      currentIndex === portfolioMetaData.length - 1 ? 0 : currentIndex + 1;
-    setPrevIndex(prev);
-    setNextIndex(next);
-    console.log("prev", prev);
-    console.log("next", next);
-  }, [currentIndex]);
-
-  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (!isClickable) {
-      e.preventDefault();
+    if (nowIndex !== -1) {
+      setCurrentMenu(currentPath);
+      setCurrentIndex(nowIndex);
     }
-  };
+  }, [router]);
+
+  const prevIndex =
+    currentIndex === 0 ? portfolioMetaData.length - 1 : currentIndex - 1;
+  const nextIndex =
+    currentIndex === portfolioMetaData.length - 1 ? 0 : currentIndex + 1;
 
   return (
     <>
       <div className="w-full flex justify-between mt-20">
-        <a href={portfolioMetaData[prevIndex].path} onClick={handleLinkClick}>
-          이전 페이지
-        </a>
-        <a href={portfolioMetaData[nextIndex].path} onClick={handleLinkClick}>
-          다음 페이지
-        </a>
+        <Link href={portfolioMetaData[prevIndex].path}>이전 페이지</Link>
+        <Link href={portfolioMetaData[nextIndex].path}>다음 페이지</Link>
       </div>
       <H1 text={currentMenu} />
       <H1 text={currentIndex.toString()} />
       <H1 text={prevIndex.toString()} />
       <H1 text={nextIndex.toString()} />
-      <div>{portfolioMetaData[currentIndex].path}</div>
-      <div>{portfolioMetaData[prevIndex].path}</div>
-      <div>{portfolioMetaData[nextIndex].path}</div>
     </>
   );
 };
+
 export default MenuComp;
