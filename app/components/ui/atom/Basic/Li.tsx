ed0714f088d824ae16ca7b108a21f2a8a0cd223d
YoonJunHyeon <qmdldos@gmail.com>
2024-04-19 14:19:45 +0900

    dooc : 주석 업데이트

--- app/components/ui/atom/Basic/Li.tsx
+++ app/components/ui/atom/Basic/Li.tsx
@@ -5,22 +5,25 @@ interface DivProps extends OnClickProps {
   className?: string; // Add className prop
 }
 
+// Li 컴포넌트 정의
 /**
  * 리스트 아이템 컴포넌트입니다.
+ *
+ * @component
  * @example
  * ```tsx
- * <Li>리스트 아이템</Li>
+ * // 사용 예시
+ * <Li className="customLi" onClick={handleClick}>
+ *   List Item
+ * </Li>
  * ```
  *
- *
- * @param {React.FC<DivProps>} props - 컴포넌트 속성
- * @param {React.ReactNode} props.children - 자식 요소
- * @param {string} props.className - 클래스 이름
+ * @param {Object} props - 컴포넌트 속성
+ * @param {React.ReactNode} props.children - 리스트 아이템 내용
+ * @param {string} [props.className=""] - 추가적인 클래스 이름
+ * @param {Function} props.onClick - 클릭 이벤트 핸들러 함수
  * @returns {React.ReactElement} 리스트 아이템 컴포넌트
- *
- *
  */
-// Li 컴포넌트 정의
 const Li: React.FC<DivProps> = ({ children, className = "", onClick }) => {
   // 기본 스타일 초기화 및 사용자 정의 클래스 적용
   const liStyle = {
