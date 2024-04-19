9d412d8dcfea2373bed6883f64faff9b85969bdf
YoonJunHyeon <qmdldos@gmail.com>
2024-04-19 15:44:33 +0900

    Docs : Todo 추가(에러 사진 관련), 처리된 주석 삭제

--- app/components/ui/Molecules/ImgCard/IntroMenuImage.tsx
+++ app/components/ui/Molecules/ImgCard/IntroMenuImage.tsx
@@ -6,8 +6,9 @@ import Image from "next/image";
 export default function IntroMenuImage({ item }: { item: PortfolioMetaData }) {
   return (
     <div className="rightAlignImgCard flex flex-col justify-end items-end gap-4 Testing">
+      {/* todo : 기본값을 에러 사진으로 대체해야 한다. */}
       <Image
-        src={item.introduce.imgUrl || "사진 주소가 없습니다."}
+        src={item.introduce.imgUrl || "/images/entry.png"}
         alt="이미지 설명"
         width={450}
         height={220}
@@ -15,7 +16,7 @@ export default function IntroMenuImage({ item }: { item: PortfolioMetaData }) {
       />
       <div className="ImageCardTextArea max-w-[600px]">
         <p className="text-right leading-6 text-[14px]">
-          {item.introduce.content || "내용이 없습니다."}
+          {item.introduce.content || "설명이 없습니다."}
         </p>
       </div>
     </div>
--- app/components/ui/Molecules/Menu/IntroMenu.tsx
+++ app/components/ui/Molecules/Menu/IntroMenu.tsx
@@ -5,11 +5,6 @@ import { useState } from "react";
 import IntroMenuImage from "@/app/components/ui/Molecules/ImgCard/IntroMenuImage";
 import IntroMenuList from "@/app/components/ui/Molecules/Lists/IntroMenuList";
 
-// // todo 이 코드의 단점을 개선
-// // 1. LI컴포넌트를 2번 렌더링 한다.
-// // 2. 코드가 한군데에 모여있다.
-// // 3. 에러 처리를 개선해야 한다.
-
 const IntroMenu = () => {
   // portfolioMetaData가 비어있지 않은 경우 첫 번째 아이템을, 비어있는 경우 기본값을 설정
   const [selectedItem, setSelectedItem] = useState(
@@ -33,7 +28,6 @@ const IntroMenu = () => {
         <div className="w-1/4">
           <h1 className="koulen text-start titleTypo text-[80px]">Intro</h1>
         </div>
-        {/* menulist 자리 */}
         <IntroMenuList
           items={portfolioMetaData}
           onItemClick={handleItemClick}
