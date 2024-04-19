c3b96eaf2a0b06cf1962285dcf14a70596576656
YoonJunHyeon <qmdldos@gmail.com>
2024-04-19 15:35:55 +0900

    Issue : 컴포넌트 적용 단계에서 TS 타입 에러가 발생하여 수정 중이다.
    opitional 연산자를 사용한 것이 원인일까? 삭제도 해보고 추가도 해보고, 기본 값도 추가해보았지만 계속 같은 오류가 발생한다.
    
    42번 줄, Item 파라미터 인터페이스 오류인 것 같다.

--- app/components/ui/Molecules/Menu/IntroMenu.tsx
+++ app/components/ui/Molecules/Menu/IntroMenu.tsx
@@ -1,39 +1,46 @@
+"use client";
+
 import portfolioMetaData from "@/app/_constants/portfolio/portfolioMetaData";
-import Li from "@/app/components/ui/Atom/Basic/Li";
-import Ul from "@/app/components/ui/Atom/Basic/Ul";
-import Image from "next/image";
+import { useState } from "react";
+import IntroMenuImage from "@/app/components/ui/Molecules/ImgCard/IntroMenuImage";
+import IntroMenuList from "@/app/components/ui/Molecules/Lists/IntroMenuList";
+
+// // todo 이 코드의 단점을 개선
+// // 1. LI컴포넌트를 2번 렌더링 한다.
+// // 2. 코드가 한군데에 모여있다.
+// // 3. 에러 처리를 개선해야 한다.
 
 const IntroMenu = () => {
+  // portfolioMetaData가 비어있지 않은 경우 첫 번째 아이템을, 비어있는 경우 기본값을 설정
+  const [selectedItem, setSelectedItem] = useState(
+    portfolioMetaData[0] || {
+      name: "",
+      path: "",
+      stack: [],
+      introduce: { imgUrl: "", content: "" },
+    }
+  );
+
+  // 메타데이터에서 쓰인 아이템을 클릭했을 때, 해당 아이템을 선택하는 함수
+  const handleItemClick = (item: (typeof portfolioMetaData)[number]) => {
+    console.log("item : ", item);
+    setSelectedItem(item);
+  };
+
   return (
-    <div className="IntroMenu flex justify-end w-full gap-4 Li Testing">
-      <div className="w-1/4">
-        <h1 className="koulen text-start titleTypo text-[80px]">Intro</h1>
-      </div>
-      <Ul className="flex items-start justify-start gap-12 koulen text-4xl ">
-        {portfolioMetaData.map((item, index) => (
-          <Li key={index} className="Li typo">
-            {item.name}
-          </Li>
-        ))}
-      </Ul>
-      {/* 이미지 카드 파트 */}
-      <div className="rightAlignImgCard flex flex-col justify-end items-end gap-4 TeWsting">
-        <Image
-          src={
-            portfolioMetaData[0]?.introduce?.imgUrl || "사진 주소가 없습니다."
-          }
-          alt="이미지 설명"
-          width={450}
-          height={220}
-          className="ImageCardImg Testing flex justify-end"
-        />
-        <div className="ImageCardTextArea max-w-[600px]">
-          <p className="text-right leading-7 text-[14px]">
-            {portfolioMetaData[0]?.introduce?.content || "내용이 없습니다."}
-          </p>
+    <>
+      <div className="IntroMenu flex justify-end w-full gap-4 Li Testing">
+        <div className="w-1/4">
+          <h1 className="koulen text-start titleTypo text-[80px]">Intro</h1>
         </div>
+        {/* menulist 자리 */}
+        <IntroMenuList
+          items={portfolioMetaData}
+          onItemClick={handleItemClick}
+        />
       </div>
-    </div>
+      <IntroMenuImage item={selectedItem} />
+    </>
   );
 };
 
