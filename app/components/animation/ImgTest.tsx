c38b24f44288b2d6ee3549209d54dbe532d11aba
YoonJunHyeon <qmdldos@gmail.com>
2024-04-23 11:10:09 +0900

    test : img 태그를 활용

--- app/components/animation/ImgTest.tsx
+++ app/components/animation/ImgTest.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
 // todo : 해당 컴포넌트에는 프로필 이미지에 관련된 애니메이션 효과를 구현한다.
-// 1. root div는 효과 범위로 결정한다.
-// 2. 가운데 백그라운드 전용 div를 생성
-// 3. root div에 한하여 마우스 반대 방향으로 움직이는 div를 생성하여 이 div안에만 컨텐츠 들이 보여야 한다.
+// 1. 부모 div가 존재합니다.
+// 2. 사진 영역 div가 존재합니다.
+// 3. 이 사진 영역에 이미지가 존재합니다. 이 이미지는 고정되어 있습니다.
+// 4. 이 사진영역을 위에 div가 따로 존재합니다. 이 div를 통해서만 이미지가 보여야 합니다.
+// 5. 보여지는 div는 부모 div의 마우스에 따라 이동되게 됩니다.
 
 export default function ImgTest() {
-  return <div>imgtest component</div>;
+  return (
+    <div className="w-full h-full absolute overflow-hidden pointer-events-none Testing">
+      <img
+        src="/images/kakao-talk-202402182120487351.png"
+        alt="Follow the Mouse"
+        className="absolute"
+      />
+    </div>
+  );
 }
