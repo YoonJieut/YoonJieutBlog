a06613073f0f68c7a113ab49602982314157d4de
YoonJunHyeon <qmdldos@gmail.com>
2024-04-23 13:20:28 +0900

    init : CSS 마스킹 기술을 활용한 애니메이션 구현을 위한 초기 세팅

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -3,7 +3,9 @@ import ImgTest from "@/app/components/animation/ImgTest";
 const TestPage = () => {
   return (
     <main className="w-screen h-screen flexCenter">
-      <ImgTest />
+      <div className="ImgArea w-[500px] h-[500px]">
+        <ImgTest />
+      </div>
     </main>
   );
 };
