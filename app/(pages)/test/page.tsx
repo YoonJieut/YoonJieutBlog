92e8959c3fd4b7c5e93bca057743461685a051e0
YoonJunHyeon <qmdldos@gmail.com>
2024-04-25 15:43:31 +0900

    init : 슈도 코드 및 애니메이션 주석 구체화, 적용 test 완료

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,9 +1,9 @@
-import InteractiveClipPath from "@/app/components/animation/InteractiveClipPath";
+import UnderBarAni from "@/app/components/animation/UnderBarAni";
 
 const TestPage = () => {
   return (
     <main className="w-screen h-screen flexCenter">
-      <InteractiveClipPath />
+      <UnderBarAni />
     </main>
   );
 };
--- app/components/animation/UnderBarAni.tsx
+++ app/components/animation/UnderBarAni.tsx
@@ -1,7 +1,28 @@
 // UnnderBarAni.tsx
 // children에 있는 길이에 맞춰 호버 했을 때, 언더바 애니메이션이 생성됩니다.
 
-// 슈도코드
-// 1. children인 REACT NODE의 길이를 측정합니다.
-// 2. 마우스가 호버되면 애니메이션을 생성합니다.
-// 이 애니메이션은 길이에 따라 변하며, 왼쪽에서 오른쪽으로 1000% 꽉 차는 애니메이션이 구현됩니다.
+"use client";
+
+import React, { useRef, useEffect } from "react";
+
+type UnderBarAniProps = {
+  children: React.ReactNode;
+};
+
+// ? style-in-jsx를 활용하면 내부에 스타일 태그 생성 가능
+
+// * JS를 활용한 언더바 애니메이션
+// 조건 : 자식 노드를 활용한다.
+// 1. MOUSEENTER 할 때, 0~100% 길이로 언더바가 생성된다.
+// 2. MOUSELEAVE 할 때, 100%~0 길이로 언더바가 사라진다.
+
+// * 구현 방법
+// 1. children을 인지한다.
+// 2. 길이를 측정한다.
+// 3. 마우스 enter, leave 이벤트를 생성한다.
+// 4. 애니메이션을 생성한다. (width를 0에서 100%로 변경하고 그와 반대로 실행)
+
+const UnderBarAni = () => {
+  return <>helloWorld</>;
+};
+export default UnderBarAni;
