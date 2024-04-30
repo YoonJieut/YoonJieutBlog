e5e0ad4aa8cf3272ef87f9104255ada25ee13ac2
YoonJunHyeon <qmdldos@gmail.com>
2024-04-30 16:47:34 +0900

    Docs : 리터럴 추출 작업 컴포넌트
    본 컴포넌트에서 리터럴 추출 작업을 진행

--- app/components/ui/Page/aboutme/AboutmePage.tsx
+++ app/components/ui/Page/aboutme/AboutmePage.tsx
@@ -12,16 +12,14 @@ const AboutmePage: React.FC = () => {
   return (
     <div className="aboutMe flex gap-2 w-full h-full relative">
       <Section className="w-1/4 h-full p-4 flex flex-col gap-10 bg-sub-1 font-basic overflow-hidden">
-        <div className="w-full relative flex flex-row items-start justify-between text-left text-[0.75rem] text-darkslategray font-noto-sans-kr">
+        <div className="w-full relative flex flex-row items-start justify-between  text-[0.75rem]">
           <div className="w-[10.188rem] relative h-[8.063rem]">
             <div className="absolute top-[2.25rem] left-[0rem] w-[10.188rem] h-[5.813rem]">
-              <div className="absolute h-[85.22%] w-full top-[0%] left-[0%] leading-[184%] inline-block">
-                <p className="m-0">{process.env.USER_BIRTHDAY}</p>
-                <p className="m-0">
-                  현 재 지 역 : {aboutmeMessage[0].USER_NAME?.Region}
-                </p>
-                <p className="m-0">{process.env.USER_PHONE}</p>
-                <p className="m-0">
+              <div className="absolute h-[85.22%] w-full top-[0%] left-[0%] leading-[184%]">
+                <p>{process.env.USER_BIRTHDAY}</p>
+                <p>현 재 지 역 : {aboutmeMessage[0].USER_NAME?.Region}</p>
+                <p>{process.env.USER_PHONE}</p>
+                <p>
                   {/* . noopener 속성을 사용하면 새 창에서 열린 문서가 원래 문서에 대한 참조를 가지지 않게 함 */}
                   {/* HTTP 리퍼러 헤더는 사용자가 어떤 웹 페이지에서 링크를 클릭했는지를 나타내는 정보를 차단하는 noreferrer를 사용 */}
                   <a
@@ -32,7 +30,7 @@ const AboutmePage: React.FC = () => {
                     Github : {process.env.USER_GITHUB}
                   </a>
                 </p>
-                <p className="m-0">{process.env.USER_EMAIL}</p>
+                <p>{process.env.USER_EMAIL}</p>
               </div>
             </div>
             <div className="absolute top-[0rem] left-[0rem] w-[4.188rem] h-[1.688rem] text-[1rem]">
@@ -50,42 +48,43 @@ const AboutmePage: React.FC = () => {
           />
         </div>
         {/* 기술 */}
-        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem] text-left text-[1rem] text-darkslategray font-noto-sans-kr">
+        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem]  text-[1rem]  ">
           <div className="w-[4.188rem] relative h-[1.688rem]">
             <div className="absolute h-[3.7%] w-full top-[96.3%] right-[0%] bottom-[0%] left-[0%] bg-darkslategray" />
             <b className="absolute top-[0%] left-[0%] leading-[101%]">기술</b>
           </div>
           <div className="w-[18.375rem] relative h-[5.5rem] text-[0.75rem]">
             <div className="absolute h-[85.22%] w-full top-[0%] left-[0%] leading-[94%] inline-block">
-              <p className="m-0 ">
-                Frontend : {aboutmeMessage[1].기술?.FrontEnd}
-              </p>
-              <p className="m-0">&nbsp;</p>
-              <p className="m-0">BackEnd : {aboutmeMessage[1].기술?.BackEnd}</p>
-              <p className="m-0">&nbsp;</p>
+              <p>Frontend : {aboutmeMessage[1].기술?.FrontEnd}</p>
+              <p>&nbsp;</p>
+              <p>BackEnd : {aboutmeMessage[1].기술?.BackEnd}</p>
+              <p>&nbsp;</p>
               <p>DevOps : {aboutmeMessage[1].기술?.DevOps}</p>
-              <p className="m-0">&nbsp;</p>
-              <p className="m-0">ETC : {aboutmeMessage[1].기술?.ETC}</p>
+              <p>&nbsp;</p>
+              <p>ETC : {aboutmeMessage[1].기술?.ETC}</p>
             </div>
           </div>
         </div>
         {/* 교육 */}
-        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem] text-left text-[1rem] text-darkslategray font-noto-sans-kr">
+        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem]  text-[1rem]  ">
           <div className="w-[4.188rem] relative h-[1.688rem]">
             <div className="absolute h-[3.7%] w-full top-[96.3%] right-[0%] bottom-[0%] left-[0%] bg-darkslategray" />
             <b className="absolute top-[0%] left-[0%] leading-[101%]">교육</b>
           </div>
           <div className="w-[18.375rem] relative text-[0.75rem] leading-[184%] inline-block shrink-0">
-            <p className="m-0">지역인재양성을 위한 IaaS기반 웹 서비스</p>
-            <p className="m-0">(Reat,Node,Next,MLOps)개발자 과정</p>
-            <p className="m-0">2023.08 ~ 2024.03</p>
-            <p className="m-0">&nbsp;</p>
-            <p className="m-0">웹 앱 및 프론트엔드개발자(react)</p>
-            <p className="m-0">2022.10 ~ 2023.03</p>
+            {aboutmeMessage[2].교육?.Iaas.map((item: string, index: number) => (
+              <p key={index}>{item}</p>
+            ))}
+            <p>&nbsp;</p>
+            {aboutmeMessage[2].교육?.Publisher.map(
+              (item: string, index: number) => (
+                <p key={index}>{item}</p>
+              )
+            )}
           </div>
         </div>
 
-        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem] text-left text-[0.75rem] text-darkslategray font-noto-sans-kr">
+        <div className="relative w-full flex flex-col items-start justify-start gap-[0.625rem_0rem] text-[0.75rem]  ">
           <div className="w-[4.188rem] relative h-[1.688rem] text-[1rem]">
             <div className="absolute h-[3.7%] w-full top-[96.3%] right-[0%] bottom-[0%] left-[0%] bg-darkslategray" />
             <b className="absolute top-[0%] left-[0%] leading-[101%]">
@@ -94,21 +93,15 @@ const AboutmePage: React.FC = () => {
           </div>
           <div className="w-[18.375rem] relative h-[3.063rem]">
             <div className="absolute h-[85.22%] w-full top-[0%] left-[0%] leading-[184%] inline-block">
-              <p className="m-0">우송정보대학 (2,3년) 졸업</p>
-              <p className="m-0">언어치료과 / 사회복지학과</p>
+              <p>{process.env.USER_UNIVERSITY}</p>
+              <p>{process.env.USER_GRADUATION_MAJOR}</p>
             </div>
           </div>
           <div className="w-[18.375rem] relative h-[4.063rem]">
             <div className="absolute h-[85.22%] w-full top-[0%] left-[0%] leading-[184%] inline-block">
-              <p className="m-0">
-                웹 디자인 기능사 (2023.12) - 한국산업인력공단
-              </p>
-              <p className="m-0">
-                사회복지사 2급(2023.04) - 한국사회복지사협회
-              </p>
-              <p className="m-0">
-                1종보통운전면허 (2015.03) - 경찰청(운전면허시험관리단)
-              </p>
+              {aboutmeMessage[3].자격증?.map((item: string, index: number) => (
+                <p key={index}>{item}</p>
+              ))}
             </div>
           </div>
         </div>
