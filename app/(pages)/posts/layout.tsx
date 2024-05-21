8c51cb066b154cea0335f317665c14abfe7f7a4f
YoonJieut <qmdldos@gmail.com>
2024-05-21 21:50:08 +0900

    Issue Fix : @를 활용한 병렬 로딩은 layout Props에 암묵적으로 추가되는 것으로 보여진다. 이때 이 컴포넌트를 layout의 Parameter에 추가해주어야 한다.

--- app/(pages)/posts/layout.tsx
+++ app/(pages)/posts/layout.tsx
@@ -1,11 +1,15 @@
+"use client";
+import React from "react";
 import MainFooter from "@/app/components/ui/Organism/Footer/MainFooter";
 import PostsHeader from "@/app/components/ui/Organism/Header/PostsHeader";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+  postsTable,
+}: {
   children: React.ReactNode;
-}>) {
+  postsTable: React.ReactNode;
+}) {
   return (
     <div className="postsRoot w-screen h-screen flex flex-col overflow-hidden pb-[10px]">
       <PostsHeader />
