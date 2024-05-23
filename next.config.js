e95fb558edddd360b8c5531f393d0a96e046b085
YoonJunHyeon <qmdldos@gmail.com>
2024-05-23 17:56:07 +0900

    chore : 빌드 전 컴파일러 세팅
    next.config : 에러 콘솔을 제외하는 코드를 추가
    
    package : tsc 컴파일러 명령어 스크립트에 추가, window에 사용될 명령어로 sciprt 명령어 수정
    
    tsconfig : 빌드 관련 옵션 변경

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
