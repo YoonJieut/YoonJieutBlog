e06df9ae2300db47e5853caef8b7262c4be369d2
YoonJunHyeon <qmdldos@gmail.com>
2024-06-05 16:20:44 +0900

    fix : 중첩 레이아웃 오류

--- app/(pages)/main/layout.tsx
+++ app/(pages)/main/layout.tsx
@@ -3,16 +3,12 @@ import IntroduceLayout from "@/app/components/ui/Page/introduce/IntroduceLayout"
 
 export default function MainLayout() {
   return (
-    <html lang="ko">
-      <body>
-        <div
-          id="MainRoot"
-          className="w-screen h-screen overflow-hidden flex flex-col p-2 space-y-[10px]"
-        >
-          <IntroduceLayout />
-          <MainFooter />
-        </div>
-      </body>
-    </html>
+    <div
+      id="MainRoot"
+      className="w-screen h-screen overflow-hidden flex flex-col p-2 space-y-[10px]"
+    >
+      <IntroduceLayout />
+      <MainFooter />
+    </div>
   );
 }
