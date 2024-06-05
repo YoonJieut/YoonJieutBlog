b43a1111fb96f580826ab14472dd57a7e6047cba
YoonJunHyeon <qmdldos@gmail.com>
2024-06-05 15:55:27 +0900

    Docs : parallex Routes 파라미터 정의
    빌드에 실패한 병렬 로딩

--- app/(pages)/main/introduce/(portfolio)/layout.tsx
+++ app/(pages)/main/introduce/(portfolio)/layout.tsx
@@ -1,16 +1,19 @@
 import Section from "@/app/components/ui/atom/Basic/Section";
-import MenuComp from "./@menuComp/page";
+// import MenuComp from "./@menuComp/page";
 
 export default function PortfolioLayout({
+  menuComp,
   children,
 }: {
+  menuComp: React.ReactNode;
   children: React.ReactNode;
 }) {
   return (
     <div className="w-full h-11/12 flex Tesing">
       <Section className="portfolioLeftSection w-1/2 h-full">
-        <MenuComp />
+        {menuComp}
       </Section>
+
       <Section className="portfolioRightSection w-1/2  flex flex-col gap-[300px] overflow-x-hidden overflow-y-scroll text-[0.875rem] leading-[120%]">
         {children}
       </Section>
