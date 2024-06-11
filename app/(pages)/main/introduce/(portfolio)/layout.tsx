01b6207d9b2f841837180aec56f18e15d87d3cff
YoonJunHyeon <qmdldos@gmail.com>
2024-06-11 23:56:08 +0900

    test : 병령 로딩 파트를 일반 컴포넌트 호출로 수정

--- app/(pages)/main/introduce/(portfolio)/layout.tsx
+++ app/(pages)/main/introduce/(portfolio)/layout.tsx
@@ -1,17 +1,15 @@
 import Section from "@/app/components/ui/atom/Basic/Section";
-// import MenuComp from "./@menuComp/page";
+import MenuComp from "./@menuComp/page";
 
 export default function PortfolioLayout({
-  menuComp,
   children,
 }: {
-  menuComp: React.ReactNode;
   children: React.ReactNode;
 }) {
   return (
     <div className="w-full h-11/12 flex">
       <Section className="portfolioLeftSection w-1/2 h-full">
-        {menuComp}
+        <MenuComp />
       </Section>
 
       <Section className="portfolioRightSection w-1/2  flex flex-col gap-[300px] overflow-x-hidden overflow-y-scroll text-[0.875rem] leading-[120%]">
