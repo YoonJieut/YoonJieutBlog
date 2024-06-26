41d23a8380119be6d69c651d5475685e99345422
YoonJunHyeon <qmdldos@gmail.com>
2024-06-26 23:50:50 +0900

    chore : todo 주석 삭제

--- app/(pages)/main/introduce/(portfolio)/chatnovel/page.tsx
+++ app/(pages)/main/introduce/(portfolio)/chatnovel/page.tsx
@@ -1,6 +1,5 @@
 import ChatnovelContent from "@/app/components/ui/Page/portfolio/chatnovel/ChatNovelContent";
 
-// todo arrowbtn에 들어갈 기능 추가
 export default function ChatNovelPage() {
   return <ChatnovelContent />;
 }
--- app/(pages)/main/introduce/(portfolio)/erp/page.tsx
+++ app/(pages)/main/introduce/(portfolio)/erp/page.tsx
@@ -1,6 +1,5 @@
 import ErpContent from "@/app/components/ui/Page/portfolio/erp/ErpContent";
 
-// todo arrowbtn에 들어갈 기능 추가
 export default function ErpPage() {
   return <ErpContent />;
 }
